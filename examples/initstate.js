5d8a9760c8667577aba38e3e2bd01e3f7efb578d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-17 22:06:17 +0400

    fix

--- examples/initstate.js
+++ examples/initstate.js
@@ -1,5 +1,5 @@
 module.exports = key => {
-  const item = window.localStorage.getItem(key)
+  const item = window.localStorage.getItem(key) || void 0
   var initState = item && JSON.parse(item)
   const proxy$ = {
     next: r => {
