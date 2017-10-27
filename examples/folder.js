ed28a7c9a3999487cd6181cd3f6fb9f6780ccccb
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-27 21:28:33 +0400

    refactored folder example

--- examples/folder.js
+++ examples/folder.js
@@ -1,26 +1,25 @@
 const m = require('most')
 const watch$ = require('./watch$')
 const {join: pathJoin} = require('path')
-const eq = require('../src/eq')
 const PatchBark = require('../src/barks/patch')
 
 const mapError = err => m.of(put => {
-  put.node('div', {style: {color: 'red'}}, put => put.text(err.message))
+  put.node('div', {style: {color: 'red'}}, put => {
+    put.text(err.message)
+  })
 })
 
 const Folder = path => (state, sselect) => {
-  const change$ = watch$(path)
-    .skipRepeatsWith(eq)
-    .multicast()
-  const opAction$ = sselect.action$
+  state.val('@', sselect.action$
     .filter(x => x.action[0] === path)
     .map(x => x.action.slice(1))
-  state.val('@', opAction$.map(([name, isOpen]) => s =>
-    Object.assign({}, s, {[name]: isOpen})
+    .map(([name, open]) => s => Object.assign({}, s, {[name]: open})
   ).startWith(s => s || {}))
 
-  return change$.map(dir => put => {
-    put.node('ul', { style: { listStyleType: 'none' } }, (put, select) => {
+  return watch$(path).map(dir => put => {
+    put.node('ul', {
+      style: { listStyleType: 'none' }
+    }, (put, select) => {
       for (let name in dir) {
         let stat = dir[name]
         let epath = pathJoin(path, name)
@@ -32,10 +31,14 @@ const Folder = path => (state, sselect) => {
             put.node('div', isOpen$.map(op => (
                 op
                 ? put => {
-                  put.node('button', {on: {click: actClose}}, put => put.text('- ' + name))
+                  put.node('button', {
+                    on: { click: actClose }
+                  }, put => put.text('- ' + name))
                   put.onode(epath, 'div', {}, Folder(epath))
                 }
-                : put => put.node('button', {on: {click: actOpen_}}, put => put.text('+ ' + name))
+                : put => put.node('button', {
+                  on: { click: actOpen_ }
+                }, put => put.text('+ ' + name))
               ))
             )
           } else {
