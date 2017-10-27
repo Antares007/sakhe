6f6134fc22816059c65326a1e3f3bc70ce0ec86a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-10-27 21:53:41 +0400

    store formatted state

--- examples/initstate.js
+++ examples/initstate.js
@@ -2,7 +2,7 @@ module.exports = key => {
   const item = window.localStorage.getItem(key) || void 0
   var initState = item && JSON.parse(item)
   const stateCb = newState => {
-    window.localStorage.setItem(key, JSON.stringify(newState))
+    window.localStorage.setItem(key, JSON.stringify(newState, null, '  '))
     return newState
   }
   return [initState, stateCb]
