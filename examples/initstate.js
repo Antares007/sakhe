83ed43cc7625d2f97b1672348b582e8f33e7c76d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-11-01 12:11:58 +0400

    try parse state

--- examples/initstate.js
+++ examples/initstate.js
@@ -1,9 +1,13 @@
-module.exports = key => {
-  const item = window.localStorage.getItem(key) || void 0
-  var initState = item && JSON.parse(item)
-  const stateCb = newState => {
-    window.localStorage.setItem(key, JSON.stringify(newState, null, '  '))
-    return newState
-  }
-  return [initState, stateCb]
+module.exports = (key) => {
+  var initState
+  try {
+    initState = JSON.parse(window.localStorage.getItem(key)) || void 0
+  } catch (err) {}
+  return [
+    initState,
+    newState => window.localStorage.setItem(
+      key,
+      JSON.stringify(newState, null, '  ')
+    )
+  ]
 }
