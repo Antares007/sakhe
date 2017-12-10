c2c0209519bb6c4f0016f085db1e1c716e2d0938
Archil Bolkvadze <a.bolkvadze@gmail.com>
2017-12-10 11:37:22 +0400

    add skip reimplement reduce

--- src/chain.ts
+++ src/chain.ts
@@ -8,6 +8,7 @@ import {
   scan,
   startWith,
   filter,
+  skip,
   skipRepeats,
   skipRepeatsWith
 } from '@most/core'
@@ -21,25 +22,18 @@ export const chain = <A>($: Stream<A>) => ({
   scan: <B>(f: (b: B, a: A) => B, b: B) => chain(scan(f, b, $)),
   map: <B>(f: (a: A) => B) => chain(map<A, B>(f, $)),
   filter: (p: (a: A) => boolean) => chain(filter(p, $)),
+  skip: (n: number) => chain(skip(1, $)),
   skipRepeats: () => chain(skipRepeats($)),
   skipRepeatsWith: (f: (l: A, r: A) => boolean) => chain(skipRepeatsWith(f, $)),
-  reduce: <S>(f: (s: S, a: A) => S, s: S): Promise<S> => {
-    var state: S
-    return runEffects(
-      tap(
-        s => {
-          state = s
-        },
-        scan(
-          (s, a) => {
-            return s
-          },
-          s,
-          $
-        )
-      ),
-      newDefaultScheduler()
-    ).then(() => state)
+  reduce: <S>(f: (s: S, a: A) => S, initState: S): Promise<S> => {
+    var state = initState
+    return chain($)
+      .scan(f, initState)
+      .tap(s => {
+        state = s
+      })
+      .drain()
+      .then(() => state)
   },
   drain: () => runEffects($, newDefaultScheduler()),
   valueOf: () => $
