9a42816c1d96cdd3cb48dafdc991322e169afa4a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-01-01 21:40:13 +0400

    return to counter

--- src/vtree2/test.ts
+++ src/vtree2/test.ts
@@ -2,7 +2,7 @@ import {Pith, tree as rvnodeTree} from './vtree2'
 import {Data} from './types'
 import {$} from '../most'
 import {chain} from '../chain'
-import {periodic, scan} from '@most/core'
+import {periodic, scan, map} from '@most/core'
 import {toVNode} from './to-vnode'
 
 const Counter = (d = 4): Pith => (put, on) => {
@@ -37,11 +37,76 @@ const tree = <Tag extends keyof HTMLElementTagNameMap>(
 }
 
 var rez = tree<'div'>(<HTMLDivElement>document.getElementById('root-node')!)(
-  chain(periodic(1000 / 60))
-    .scan(c => c + 1, 0)
-    .map(i => i % 5)
-    .map(d => Counter(d))
-    .valueOf()
+  Counter2(2)
 )
 
 chain(rez).drain()
+
+function Counter2(d = 0): Pith {
+  return (put, on) => {
+    const pi2 = Math.PI * 2
+    const cycle$ = chain(periodic(1000 / 60)).scan(
+      i => (i >= pi2 ? 0 : i + 0.15),
+      0
+    )
+    const sum$ = chain(on)
+      .map(x => x.action)
+      .scan((sum, v) => sum + v, 0)
+    const r = 10
+    put.node('div', {style: {padding: '5px 10px', textAlign: 'center'}})(
+      put => {
+        put.node(
+          'button',
+          map(
+            i => ({
+              on: {click: +1},
+              style: {
+                position: 'relative',
+                outline: 'none',
+                'border-radius': Math.abs(Math.floor(Math.sin(i) * 20)) + 'px',
+                left: Math.floor(Math.sin(i) * r) + 'px',
+                top: Math.floor(Math.cos(i) * r) + 'px',
+                'background-color': `rgb(${100 +
+                  d * 20 +
+                  Math.floor(30 * Math.sin(i))}, ${100 +
+                  d * 20 +
+                  Math.floor(30 * Math.cos(i))}, 255)`
+              }
+            }),
+            cycle$.valueOf()
+          )
+        )(put => {
+          put.node('span', {})(put => put.text('+'))
+          if (d > 0) put.node('div', {})(Counter2(d - 1))
+        })
+        put.node(
+          'button',
+          map(
+            i => ({
+              on: {click: -1},
+              style: {
+                position: 'relative',
+                outline: 'none',
+                'border-radius': Math.abs(Math.floor(Math.cos(i) * 20)) + 'px',
+                left: Math.floor(Math.cos(i) * r) + 'px',
+                top: Math.floor(Math.sin(i) * r) + 'px',
+                'background-color': `rgb(255, ${100 +
+                  d * 20 +
+                  Math.floor(30 * Math.sin(i))}, ${100 +
+                  d * 20 +
+                  Math.floor(30 * Math.cos(i))})`
+              }
+            }),
+            cycle$.valueOf()
+          )
+        )(put => {
+          put.node('span', {})(put => put.text('-'))
+          if (d > 0) put.node('div', {})(Counter2(d - 1))
+        })
+        put.node('h3', {})(put =>
+          put.text(sum$.map(n => n.toString()).valueOf())
+        )
+      }
+    )
+  }
+}
