ab240b413f5e4b3cf296f0c2828d257a53880a56
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-01-01 16:56:16 +0400

    test performance

--- src/vtree2/test.ts
+++ src/vtree2/test.ts
@@ -1,34 +1,47 @@
-import {Pith, tree} from './vtree2'
+import {Pith, tree as rvnodeTree} from './vtree2'
+import {Data} from './types'
+import {$} from '../most'
 import {chain} from '../chain'
+import {periodic, scan} from '@most/core'
 import {toVNode} from './to-vnode'
 
-const Counter = (d = 3) =>
-  function(put, on) {
-    put.node('div')(put => {
-      put.node('button', {on: {click: +1}})(put => {
-        put.text('+')
-        if (d > 0) put.node('div')(Counter(d - 1))
-      })
-      put.node('button', {on: {click: -1}})(put => {
-        put.text('-')
-        if (d > 0) put.node('div')(Counter(d - 1))
-      })
+const Counter = (d = 4): Pith => (put, on) => {
+  put.node('div')(put => {
+    put.node('button', {on: {click: +1}})(put => {
+      put.text('+')
+      if (d > 0) put.node('div')(Counter(d - 1))
     })
-    put.text(
-      chain(on)
-        .map(x => x.action)
-        .scan((c, a) => c + a, 0)
-        .map(String)
-        .valueOf()
-    )
-  } as Pith
-
-var rez = tree('div')(Counter())
-
-chain(rez)
-  .scan(
-    (t, r) => r(t, e => console.log('on', e)),
-    toVNode<'div'>(document.getElementById('root-node')!)
+    put.node('button', {on: {click: -1}})(put => {
+      put.text('-')
+      if (d > 0) put.node('div')(Counter(d - 1))
+    })
+  })
+  put.text(
+    chain(on)
+      .map(x => x.action)
+      .scan((c, a) => c + a, 0)
+      .map(String)
+      .valueOf()
   )
-  // .tap(vnode => console.info('patch', JSON.stringify(vnode, null, '  ')))
-  .drain()
+}
+
+const tree = <Tag extends keyof HTMLElementTagNameMap>(
+  element: HTMLElementTagNameMap[Tag],
+  data?: Data
+) => (pith: $<Pith>) => {
+  return scan(
+    (t, r) => r(t, e => console.info(e)),
+    toVNode<Tag>(element),
+    rvnodeTree<Tag>(<Tag>element.tagName.toLowerCase(), data)(pith)
+  )
+}
+
+var rez = tree<'div'>(<HTMLDivElement>document.getElementById('root-node')!)(
+  chain(periodic(1000 / 60))
+    .scan(c => c + 1, 0)
+    .map(i => i % 5)
+    .map(d => Counter(d))
+    .valueOf()
+)
+
+chain(rez).drain()
