085af42868f40e81b463e91ede1b689a7976679b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-01-06 13:48:24 +0400

    vtree types

---
+++ src.flow/vtree/types.js
@@ -0,0 +1,43 @@
+// @flow
+export type AllEventTypes =
+  | MouseEventTypes
+  | FocusEventTypes
+  | KeyboardEventTypes
+  | TouchEventTypes
+  | WheelEventTypes
+  | ProgressEventTypes
+  | DragEventTypes
+  | AnimationEventTypes
+
+export interface Data {
+  +attrs?: {[key: string]: string | boolean | number};
+  +class?: {[key: string]: boolean};
+  +props?: {[key: string]: mixed};
+  +style?: {[key: string]: string};
+  +on?: {[key: AllEventTypes]: mixed};
+}
+
+export interface VNode {
+  type: 'node';
+  tag: string;
+  key?: string;
+  data: Data;
+  children: VTree[];
+  node: Element;
+}
+
+export interface VText {
+  type: 'text';
+  data: string;
+  node: Text;
+}
+
+export interface VComment {
+  type: 'comment';
+  data: string;
+  node: Comment;
+}
+
+export type VCharacterData = VComment | VText
+
+export type VTree = VNode | VText | VComment
