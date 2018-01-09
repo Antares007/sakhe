c04cb16ba49f6891f4b53f98b207678acfebd772
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-01-09 20:29:58 +0400

    move onChange to rtree

--- src.flow/r.js
+++ src.flow/r.js
@@ -1,37 +1,51 @@
 import {map, mergeArray} from '@most/core'
 import {tree as mostTree, to$} from './most'
+import stateProxy from './stateProxy'
 
 export default function tree(absurdA) {
+  const [on, proxy] = stateProxy()
   return pith =>
-    mostTree(mergeArray)(
+    mostTree(xs =>
+      map(
+        r => s => {
+          const ns = r(s)
+          on(ns)
+          return ns
+        },
+        mergeArray(xs)
+      )
+    )(
       map(
         pith => put => {
-          pith({
-            extend: (key, absurdB) => oPith =>
-              put(
-                map(
-                  r => a => {
-                    const ak = a[key]
-                    const bk = r(Object.assign(absurdB(), ak))
-                    if (ak === bk) return a
-                    return Object.assign(absurdA(), a, {[key]: bk})
-                  },
-                  tree(absurdB)(oPith)
-                )
-              ),
-            val: (key, r) =>
-              put(
-                map(
-                  r => a => {
-                    const ak = a[key]
-                    const bk = r(ak)
-                    if (ak === bk) return a
-                    return Object.assign(absurdA(), a, {[key]: bk})
-                  },
-                  r
-                )
-              ),
-          })
+          pith(
+            {
+              extend: (key, absurdB) => oPith =>
+                put(
+                  map(
+                    r => a => {
+                      const ak = a[key]
+                      const bk = r(Object.assign(absurdB(), ak))
+                      if (ak === bk) return a
+                      return Object.assign(absurdA(), a, {[key]: bk})
+                    },
+                    tree(absurdB)(oPith)
+                  )
+                ),
+              val: (key, r) =>
+                put(
+                  map(
+                    r => a => {
+                      const ak = a[key]
+                      const bk = r(ak)
+                      if (ak === bk) return a
+                      return Object.assign(absurdA(), a, {[key]: bk})
+                    },
+                    r
+                  )
+                ),
+            },
+            proxy
+          )
         },
         to$(pith)
       )
--- src.flow/state.js
+++ src.flow/state.js
@@ -6,43 +6,12 @@ import stateProxy from './stateProxy'
 
 export default function tree(absurdA, initState) {
   return pith => {
-    const [next, proxy] = stateProxy()
-    const rez = rTree(absurdA)(
-      map(
-        (function ring(onChange) {
-          return pith => put => {
-            pith(
-              {
-                extend: (key, absurdB) => pith => {
-                  const bKeysLenght = Object.keys(absurdB()).length
-                  const onChangeB = filter(
-                    ak =>
-                      typeof ak !== 'undefined' &&
-                      Object.keys(ak).length === bKeysLenght,
-                    map(
-                      a =>
-                        typeof a === 'object' && a !== null
-                          ? a[key]
-                          : undefined,
-                      onChange
-                    )
-                  )
-                  put.extend(key, absurdB)(ring(onChangeB)(pith))
-                },
-                val: put.val,
-              },
-              onChange
-            )
-          }
-        })(proxy),
-        to$(pith)
-      )
-    )
+    const rez = rTree(absurdA)(pith)
     return skip(
       1,
-      tap(a => {
-        if (next) next(a)
-      }, skipRepeats(scan((s, r) => r(s), initState != null ? initState : absurdA(), rez)))
+      skipRepeats(
+        scan((s, r) => r(s), initState != null ? initState : absurdA(), rez)
+      )
     )
   }
 }
