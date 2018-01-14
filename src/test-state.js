7fc326823619993ee8c2ba342331361fe60107bd
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-01-14 17:16:48 +0400

    $Exact

--- src/test-state.js
+++ src/test-state.js
@@ -5,18 +5,17 @@ import tree from './state'
 
 const abs = () => ({a: 42, b: {o: 'otar'}})
 
-const rez = tree(abs, {a: 43, b: {o: 'otar bolkvadze', t: 1}})((put, on) => {
+const rez = tree(abs)((put, on) => {
   M.of(on)
     .tap(s => global.console.log(0, JSON.stringify(s)))
     .drain()
   put.val('a', now(n => n + 1))
-  put.extend('b', () => ({o: 'archil', t: 1}))((put, on) => {
+  put.extend('b', () => ({o: 'archil', t: 1, b: 1}))((put, on) => {
     M.of(on)
       .tap(s => global.console.log(1, JSON.stringify(s)))
       .drain()
     put.val('o', now(s => `${s} bolkvadze`))
   })
-  // put.put(now(s => ({...s, a: -42})))
 })
 
 M.of(rez)
