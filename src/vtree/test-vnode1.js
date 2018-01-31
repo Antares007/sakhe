29626813ce1982bae8bd97c900e54aceda003223
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-01-31 22:05:22 +0400

    component?

--- src/vtree/test-vnode1.js
+++ src/vtree/test-vnode1.js
@@ -54,7 +54,16 @@ function Counter<T: HTMLElement>(d = 0): Pith<T> {
     const sum = M.of(proxy)
       .scan((c, a) => c + a, 0)
       .map(String).$
-
+    // dom.put(
+    //   r(dom => {
+    //     dom.node(
+    //       'h1',
+    //       constant(v => {
+    //         v.node.innerText = 'aaaa'
+    //       })
+    //     )
+    //   })
+    // )
     dom.node(
       'div',
       r(dom => {
@@ -64,56 +73,46 @@ function Counter<T: HTMLElement>(d = 0): Pith<T> {
             style.textAlign = 'center'
           })
         )
-        button(dom, 1)
-        button(dom, -1)
+        dom.node('button', button(1))
+        dom.node('button', button(-1))
       })
     )
     dom.node('h3', r(dom => dom.text(textContent(sum))))
 
-    function button(dom, action: 1 | -1 = 1) {
+    function button(action: 1 | -1 = 1): Stream<R<VNode<HTMLButtonElement>>> {
       const [wave1, wave2] =
         action > 0 ? [Math.sin, Math.cos] : [Math.cos, Math.sin]
-
-      dom.node(
-        'button',
-        r(dom => {
-          dom.put(
-            constant(({node}, scheduler) => {
-              const {style} = node
-              node.textContent = action > 0 ? '+' : '-'
-              node.className = action > 0 ? 'increment' : 'decriment'
-              style.position = 'relative'
-              style.outline = 'none'
-              const listener = (e: MouseEvent) => {
-                proxy.event(scheduler.currentTime(), action)
-              }
-              node.addEventListener('click', listener)
-              return () => node.removeEventListener('click', listener)
-            })
+      return r(dom => {
+        dom.put(
+          constant(({node}, scheduler) => {
+            const {style} = node
+            node.textContent = action > 0 ? '+' : '-'
+            node.className = action > 0 ? 'increment' : 'decriment'
+            style.position = 'relative'
+            style.outline = 'none'
+            const listener = (e: MouseEvent) => {
+              proxy.event(scheduler.currentTime(), action)
+            }
+            node.addEventListener('click', listener)
+            return () => node.removeEventListener('click', listener)
+          })
+        )
+        dom.put(
+          map(
+            i => ({node: {style}}) => {
+              style.borderRadius = `${Math.abs(Math.floor(wave1(i) * 20))}px`
+              style.left = `${Math.floor(wave1(i) * radius)}px`
+              style.top = `${Math.floor(wave2(i) * radius)}px`
+              const c1 = 100 + d * 20 + Math.floor(30 * wave1(i))
+              const c2 = 100 + d * 20 + Math.floor(30 * wave2(i))
+              style.backgroundColor =
+                action > 0 ? `rgb(${c1}, ${c2}, 255)` : `rgb(255, ${c1}, ${c2})`
+            },
+            cycle$
           )
-          dom.put(
-            map(
-              i => ({node: {style}}) => {
-                style.borderRadius = `${Math.abs(Math.floor(wave1(i) * 20))}px`
-                style.left = `${Math.floor(wave1(i) * radius)}px`
-                style.top = `${Math.floor(wave2(i) * radius)}px`
-                style.backgroundColor =
-                  action > 0
-                    ? `rgb(${100 + d * 20 + Math.floor(30 * wave1(i))}, ${100 +
-                        d * 20 +
-                        Math.floor(30 * wave2(i))}, 255)`
-                    : `rgb(255, ${100 +
-                        d * 20 +
-                        Math.floor(30 * wave1(i))}, ${100 +
-                        d * 20 +
-                        Math.floor(30 * wave2(i))})`
-              },
-              cycle$
-            )
-          )
-          if (d > 0) dom.node('div', r(Counter(d - 1)))
-        })
-      )
+        )
+        if (d > 0) dom.node('div', r(Counter(d - 1)))
+      })
     }
 
     function textContent<T: VText | VComment>(
