3776b35b0cf679a15b29f8d4a1e093896e69fe66
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-02-01 19:14:41 +0400

    clean up after yourself

--- src/vtree/test-vnode1.js
+++ src/vtree/test-vnode1.js
@@ -8,8 +8,8 @@ import {
   newStream,
   propagateEventTask,
   MulticastSource,
-  startWith,
-  never
+  never,
+  startWith
 } from '@most/core'
 import {disposeBoth, disposeWith} from '@most/disposable'
 import {asap} from '@most/scheduler'
@@ -21,28 +21,90 @@ import r, {
   type VNode,
   type VText,
   type VComment,
-  type R,
-  toVNode
+  type R
 } from '../vtree/rvnode0'
 
 const coll = document.getElementsByTagName('div')
 const elm = [...coll].find(e => e.id === 'root-node')
 if (!elm) throw new Error()
 
-const count = () =>
-  M.of(periodic(1000))
-    .scan(a => a + 1, 0)
-    .skip(1)
-    .map(i => 'h' + i)
-    .take(10)
-    .continueWith(count)
-    .valueOf()
+// const count = () =>
+//   M.of(periodic(1000))
+//     .scan(a => a + 1, 0)
+//     .map(i => i % 3)
+//     .valueOf()
 
-const rez = tree(elm)(Counter(2))
+const rez = tree(elm)(dom => {
+  const proxy = new MulticastSource(never())
+  dom.node('button', btn(0))
+  dom.node('button', btn(1))
+  dom.node('button', btn(2))
+  dom.node('button', btn(3))
+
+  function btn<T: HTMLElement>(a: number): R<VNode<T>> {
+    return r(dom => {
+      dom.put(
+        ref((v, scheduler) => {
+          v.node.innerText = a + ''
+          const listener = (e: MouseEvent) => {
+            proxy.event(scheduler.currentTime(), a)
+          }
+          v.node.addEventListener('click', listener)
+          return () => v.node.removeEventListener('click', listener)
+        })
+      )
+    })
+  }
+  dom.text(map(i => v => (v.node.textContent = i), startWith(0, proxy)))
+  dom.node('div', r(map(i => Counter2(i), startWith(0, proxy))))
+})
 
 M.of(rez)
   // .until(M.of(now(1)).delay(3000).$)
   .drain()
+
+function Counter2<T: HTMLElement>(d = 0): Pith<T> {
+  return dom => {
+    const proxy = new MulticastSource(never())
+    const sum = M.of(proxy)
+      .scan((c, a) => c + a, 0)
+      .map(String).$
+    dom.node(
+      'div',
+      r(dom => {
+        dom.node('button', button(1), 'k2')
+        dom.node('button', button(-1), 'k3')
+        dom.put(
+          ref(({node: {style}}) => {
+            style.padding = '5px 10px'
+            style.textAlign = 'center'
+          })
+        )
+      })
+    )
+    dom.node('h3', r(dom => dom.text(textContent(sum))))
+
+    function button<T: HTMLElement>(action: 1 | -1 = 1): R<VNode<T>> {
+      return r(dom => {
+        if (d > 0) dom.node('div', r(Counter2(d - 1)))
+        dom.put(
+          ref(({node}, scheduler) => {
+            const tn = document.createTextNode((action > 0 ? '+' : '-') + d)
+            node.insertBefore(tn, null)
+            node.className = action > 0 ? 'increment' : 'decriment'
+            const listener = (e: MouseEvent) =>
+              proxy.event(scheduler.currentTime(), action)
+            node.addEventListener('click', listener)
+            return () => {
+              node.removeEventListener('click', listener)
+              node.removeChild(tn)
+            }
+          })
+        )
+      })
+    }
+  }
+}
 
 function Counter<T: HTMLElement>(d = 0): Pith<T> {
   return dom => {
@@ -52,32 +114,43 @@ function Counter<T: HTMLElement>(d = 0): Pith<T> {
       .scan(i => (i >= pi2 ? 0 : i + 0.15), 0)
       .multicast().$
     const proxy = new MulticastSource(never())
-    const sum = M.of(proxy)
-      .scan((c, a) => c + a, 0)
-      .map(String).$
+
     dom.node(
       'div',
       r(dom => {
+        dom.node('button', button(1), 'k2')
+        dom.node('button', button(-1), 'k3')
         dom.put(
           ref(({node: {style}}) => {
             style.padding = '5px 10px'
             style.textAlign = 'center'
           })
         )
-        dom.node('button', button(1))
-        dom.node('button', button(-1))
-      })
+      }),
+      'k1'
     )
-    dom.node('h3', r(dom => dom.text(textContent(sum))))
+    dom.node(
+      'h3',
+      r(dom =>
+        dom.text(
+          textContent(
+            M.of(proxy)
+              .scan((c, a) => c + a, 0)
+              .map(String).$
+          )
+        )
+      )
+    )
 
-    function button<T: HTMLElement>(action: 1 | -1 = 1): Stream<R<VNode<T>>> {
+    function button<T: HTMLElement>(action: 1 | -1 = 1): R<VNode<T>> {
       const [wave1, wave2] =
         action > 0 ? [Math.sin, Math.cos] : [Math.cos, Math.sin]
       return r(dom => {
         dom.put(
           ref(({node}, scheduler) => {
             const {style} = node
-            node.textContent = action > 0 ? '+' : '-'
+            const tn = document.createTextNode(action > 0 ? '+' : '-')
+            node.appendChild(tn)
             node.className = action > 0 ? 'increment' : 'decriment'
             style.position = 'relative'
             style.outline = 'none'
@@ -85,7 +158,10 @@ function Counter<T: HTMLElement>(d = 0): Pith<T> {
               proxy.event(scheduler.currentTime(), action)
             }
             node.addEventListener('click', listener)
-            return () => node.removeEventListener('click', listener)
+            return () => {
+              node.removeEventListener('click', listener)
+              node.removeChild(tn)
+            }
           })
         )
         dom.put(
@@ -105,36 +181,32 @@ function Counter<T: HTMLElement>(d = 0): Pith<T> {
         if (d > 0) dom.node('div', r(Counter(d - 1)))
       })
     }
-
-    function textContent<T: VText | VComment>(
-      s: string | Stream<string>
-    ): Stream<R<T>> {
-      return map(
-        data => v => {
-          if (v.data === data) return
-          v.data = data
-          v.node.textContent = data
-        },
-        typeof s === 'string' ? now(s) : s
-      )
-    }
-
-    function ref<T>(
-      cf: (VNode<T>, Scheduler) => (() => void) | void
-    ): Stream<R<VNode<T>>> {
-      return newStream((sink, scheduler) => {
-        const patchedVNodes = new Map()
-        return disposeBoth(
-          asap(
-            propagateEventTask(v => {
-              if (patchedVNodes.has(v)) return
-              patchedVNodes.set(v, cf(v, scheduler))
-            }, sink),
-            scheduler
-          ),
-          disposeWith(() => patchedVNodes.forEach(d => d && d()), null)
-        )
-      })
-    }
   }
+}
+
+function ref<T>(cf: (VNode<T>, Scheduler) => (() => void) | void): R<VNode<T>> {
+  return newStream((sink, scheduler) => {
+    const patchedVNodes = new Map()
+    return disposeBoth(
+      asap(
+        propagateEventTask(v => {
+          if (patchedVNodes.has(v)) return
+          patchedVNodes.set(v, cf(v, scheduler))
+        }, sink),
+        scheduler
+      ),
+      disposeWith(() => patchedVNodes.forEach(d => d && d()), null)
+    )
+  })
+}
+
+function textContent<T: VText | VComment>(s: string | Stream<string>): R<T> {
+  return map(
+    data => v => {
+      if (v.data === data) return
+      v.data = data
+      v.node.textContent = data
+    },
+    typeof s === 'string' ? now(s) : s
+  )
 }
