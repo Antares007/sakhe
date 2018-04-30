6131e4ecc94f14e75981e5c9eca950bc873f0eff
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-04-30 17:19:39 +0400

    simplify m tree

--- core/m.fs
+++ core/m.fs
@@ -2,20 +2,10 @@
 
 open Most
 
-type M<'a> =
-    | Value of 'a
-    | OValue of Stream<'a>
-
-type Ray<'a> = M<'a> -> unit
+type Ray<'a> =  Stream<'a> -> unit
 type Pith<'a> = Ray<'a> -> unit
-
     
 let most = Most.Core.require
 
-let tree (deltac: Stream<'a>[] -> Stream<'b>) (mpith: M<Pith<'a>>): Stream<'b> =
-    let ring (pith: Pith<'a>) (ray: A.Ray<Stream<'a>>) = 
-        pith (function Value v -> ray (most.now v) | OValue ov -> ray ov)
-    let atree = A.tree deltac 
-    match mpith with
-    | Value pith -> atree (ring pith)
-    | OValue opith -> opith |> most.map (ring >> atree) |> most.switchLatest
+let tree (deltac: Stream<'a>[] -> Stream<'b>) (mpith: Stream<Pith<'a>>): Stream<'b> =
+    mpith |> most.map (A.tree deltac) |> most.switchLatest
--- core/s.fs
+++ core/s.fs
@@ -1,51 +1,34 @@
-﻿module S
-open Sakhe.M
-open Fable.Import.Browser
-
-let testPith (o: Ray<int>) = 
-    o <| Value 1
-    o <| Value 2
-    o <| Value 3
-    o <| Value 4
-
-let b = tree (most.combineArray id) <| Value testPith
-
-let c = tree most.mergeArray <| Value testPith
-
-let drain b =
-    most.runEffects (b |> most.tap console.log) (Most.Scheduler.require.newDefaultScheduler()) |> ignore
-
-drain b
-drain c
+﻿namespace Sakhe
 
 open Fable.Core
-open System
-open Fable.Core.JsInterop
 
-type AddTimeProps = 
-  abstract current : DateTime with get, set
-  [<Emit("$0.specialAmount{{=$1}}")>]
-  abstract amount : int with get, set
-  abstract unit : string with get, set
+module S =
+    open M
+    open Fable.Import.Browser
+    open Most
 
-let o = Fable.Core.JsInterop.createEmpty<AddTimeProps>
-o.amount <- 1
+    type Tag = string
+    type Key = string
+    type R<'a> = 'a -> unit
 
-[<Pojo>]
-type Person = {
-  name : string
-  age  : int
-}
+    type IRay<'a when 'a :> Element> =
+        abstract Element: tag: Tag * r: Stream<R<Element>> * ?key: Key -> unit
+        abstract Text: r: Stream<R<Text>> -> unit
+        abstract Comment: r: Stream<R<Comment>> -> unit
+        abstract Put: r: Stream<R<'a>> -> unit
 
-let p = { name = "archil"; age = 42 }
-let me = { p with name = "Zaid" }
+    type Pith<'a when 'a :> Element> = IRay<'a> -> unit
 
-type Person2 = 
-  | Name of string
-  | Age of int
+    let o = JsInterop.createEmpty<IRay<HTMLDivElement>>
 
-let person2 = [ Name "Mike"; Age 35 ]
-let mkPerson (p: Person2 list) = keyValueList CaseRules.LowerFirst p
 
-let p2 = mkPerson person2
-console.log p2
+    let tree (mpith: Stream<Pith<'a>>): Stream<'a -> unit> =
+        failwith "not implemented"
+
+
+    let rez: Stream<HTMLAnchorElement -> unit> = tree (most.now (fun o ->
+        o.Element ("div", most.now (fun x -> console.log x; ()))
+        o.Text (most.now (fun x -> console.log x; ())) 
+        o.Comment (most.now (fun x -> console.log x; ()))
+        o.Put (most.now (fun x -> console.log x; ()))
+    ))
