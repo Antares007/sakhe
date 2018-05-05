c1842b8277a6e048225e206fe515fdcc61c03c22
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-05 18:40:03 +0400

    nice

--- core/new.fs
+++ core/new.fs
@@ -22,11 +22,58 @@ type Pith<'a when 'a :> Element> = Ray<'a> -> unit
 
 let private cmb xs n = xs |> Array.iteri (fun i p -> p (n, i))
 
+let private create (f: (unit -> #Element), key: string Option) (): #Element =
+    let e = f ()
+    match key with
+    | Some _ -> e
+    | None -> e
+
+let inline private (|   IndexOutOfBounds                |
+                        SameNodeAtPosition              |
+                        SameNodeAtDifferentPosition     |
+                        OtherNodeAtPosition             |) (i, eq, node: Node) =
+    let childNodes = node.childNodes
+    let length = int childNodes.length
+
+    if i >= length then
+        IndexOutOfBounds
+    else
+        let childAtIndex = childNodes.[i]
+        if eq childAtIndex then
+            SameNodeAtPosition childAtIndex
+        else
+            let rec findNode index =
+                if index < length then
+                    let child = node.childNodes.[index]
+                    if eq child then Some child else findNode (index + 1)
+                else
+                    None
+            match findNode i with
+            | Some foundNode    -> SameNodeAtDifferentPosition (foundNode, childAtIndex)
+            | None              -> OtherNodeAtPosition childAtIndex
+
+let patch create eq patch (node: Node, index: int) =
+    match index, eq, node with
+    | IndexOutOfBounds ->
+        node.insertBefore (patch (create ())) |> ignore
+    | SameNodeAtPosition childAtIndex ->
+        patch (childAtIndex) |> ignore
+    | SameNodeAtDifferentPosition (foundNode, childAtIndex) ->
+        node.insertBefore (foundNode, childAtIndex) |> ignore
+    | OtherNodeAtPosition childAtIndex ->
+        node.insertBefore (patch (create ()), childAtIndex) |> ignore
+    
+
 let tree (pith: R<Ray<'a>>): R<'a> =
     let ring (pith: Ray<'a> -> unit) (mRay: M.Ray<'a * int -> unit>): unit =
+        let patch (cf: unit -> #Element, key: string Option, r: R<#Element>) =
+            let z = create (cf, key) ()
+            // let b = z ()
+            ()
+
         let ray (lang, key) =
             match lang with
-            | A r -> mRay (most.map (fun r (n, i) -> ()) r)
+            | A r -> patch (document.createElement_a, key, r)
             | Div r -> mRay (most.map (fun r (n, i) -> ()) r)
             | Text r -> mRay (most.map (fun r (n, i) -> ()) r)
             | Patch r -> mRay (most.map (fun r (n, i) -> ()) r)
