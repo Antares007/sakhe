ceb1e6353a495dff3bdf51690a52df235313b49c
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-09 11:10:13 +0400

    hide impl add custom case

--- core/new.fs
+++ core/new.fs
@@ -12,6 +12,7 @@ type Lang<'a when 'a :> Element> =
     | H1 of R<HTMLHeadingElement>
     | H2 of R<HTMLHeadingElement>
     | H3 of R<HTMLHeadingElement>
+    | Custom of (string * R<HTMLElement>)
     | Text of R<Text>
     | Comment of R<Comment>
     | Patch of R<'a>
@@ -21,57 +22,61 @@ type Key = string
 type Ray<'a when 'a :> Element> = Lang<'a> * Key option -> unit
 
 type Pith<'a when 'a :> Element> = Ray<'a> -> unit
-let private (|IndexOutOfBounds           |
-              SameNodeAtPosition         |
-              SameNodeAtDifferentPosition|
-              OtherNodeAtPosition        |) (index: int, eq: Node -> 'a option, node: Node) =
-    let childNodes = node.childNodes
-    let length = int childNodes.length
-    if index >= length then
-        IndexOutOfBounds
-    else
-        let childAtIndex = childNodes.[index]
-        match eq childAtIndex with
-        | Some childAtIndex -> SameNodeAtPosition childAtIndex
-        | None ->
-            let rec findNode index =
-                if index < length then
-                    match eq childNodes.[index] with
-                    | Some child -> Some child
-                    | None       -> findNode (index + 1)
-                else None
-            match findNode index with
-            | Some foundNode -> SameNodeAtDifferentPosition (foundNode, childAtIndex)
-            | None           -> OtherNodeAtPosition childAtIndex
 
-let private mkPatcher (create, eq) patch (node: #Node, index: int) =
-    match index, eq, node with
-    | IndexOutOfBounds ->
-        let child = create ()
-        patch child
-        node.appendChild child |> ignore
-        console.log ("IndexOutOfBounds", child)
-    | SameNodeAtPosition childAtIndex ->
-        patch childAtIndex |> ignore
-        console.log ("SameNodeAtPosition", childAtIndex)
-    | SameNodeAtDifferentPosition (foundNode, childAtIndex) ->
-        patch foundNode
-        node.insertBefore (foundNode, childAtIndex) |> ignore
-        console.log ("SameNodeAtDifferentPosition", foundNode)
-    | OtherNodeAtPosition childAtIndex ->
-        let child = create ()
-        patch child
-        node.insertBefore (child, childAtIndex) |> ignore
-        console.log ("OtherNodeAtPosition", child)
+module private Impl = 
+    let private (|IndexOutOfBounds           |
+                  SameNodeAtPosition         |
+                  SameNodeAtDifferentPosition|
+                  OtherNodeAtPosition        |) (index: int, eq: Node -> 'a option, node: Node) =
+        let childNodes = node.childNodes
+        let length = int childNodes.length
+        if index >= length then
+            IndexOutOfBounds
+        else
+            let childAtIndex = childNodes.[index]
+            match eq childAtIndex with
+            | Some childAtIndex -> SameNodeAtPosition childAtIndex
+            | None ->
+                let rec findNode index =
+                    if index < length then
+                        match eq childNodes.[index] with
+                        | Some child -> Some child
+                        | None       -> findNode (index + 1)
+                    else None
+                match findNode index with
+                | Some foundNode -> SameNodeAtDifferentPosition (foundNode, childAtIndex)
+                | None           -> OtherNodeAtPosition childAtIndex
 
-[<Emit "if ($1 && $0.dataset) {$0.dataset['key'] = $1}">]
-let trySetKey (_: #Element) (_: string option): unit = jsNative
+    let mkPatcher (create, eq) patch (node: #Node, index: int) =
+        match index, eq, node with
+        | IndexOutOfBounds ->
+            let child = create ()
+            patch child
+            node.appendChild child |> ignore
+            console.log ("IndexOutOfBounds", child)
+        | SameNodeAtPosition childAtIndex ->
+            patch childAtIndex |> ignore
+            console.log ("SameNodeAtPosition", childAtIndex)
+        | SameNodeAtDifferentPosition (foundNode, childAtIndex) ->
+            patch foundNode
+            node.insertBefore (foundNode, childAtIndex) |> ignore
+            console.log ("SameNodeAtDifferentPosition", foundNode)
+        | OtherNodeAtPosition childAtIndex ->
+            let child = create ()
+            patch child
+            node.insertBefore (child, childAtIndex) |> ignore
+            console.log ("OtherNodeAtPosition", child)
 
-[<Emit "$0.dataset ? $0.dataset['key'] : null">]
-let tryGetKey (_: #Element): string option = jsNative
+    [<Emit "if ($1 && $0.dataset) {$0.dataset['key'] = $1}">]
+    let trySetKey (_: #Element) (_: string option): unit = jsNative
 
-let inline mkChecker (create: unit -> 't) (nodeName: string) : (unit -> 't) * (Node -> 't option) =
-    (create, (fun n -> if n.nodeName = nodeName then Some (n :?> 't) else None))
+    [<Emit "$0.dataset ? $0.dataset['key'] : null">]
+    let  tryGetKey (_: #Element): string option = jsNative
+
+    let inline mkTypeChecker (create: unit -> 't) (nodeName: string) : (unit -> 't) * (Node -> 't option) =
+        (create, (fun n -> if n.nodeName = nodeName then Some (n :?> 't) else None))
+
+open Impl
 
 let tree (pith: R<Ray<'a>>): R<'a> =
     let ring (pith: Ray<'a> -> unit) (o: M.Ray<'a * int -> unit>): unit =
@@ -82,22 +87,23 @@ let tree (pith: R<Ray<'a>>): R<'a> =
             let checkKey (no: #HTMLElement option) =
                 no |> Core.Option.bind (fun n -> if (tryGetKey n) = key then Some n else None)
             let mape (f: unit -> #HTMLElement) nodeName =
-                mkChecker f nodeName
+                mkTypeChecker f nodeName
                 |> fun (create, eq) -> (create >> setKey, eq >> checkKey)
                 |> mkPatcher
                 |> most.map
             let mapc f nodeName =
-                mkChecker f nodeName
+                mkTypeChecker f nodeName
                 |> mkPatcher
                 |> most.map
             match lang with
-            | A r       -> r |> mape document.createElement_a               "A"        |> o
-            | H1 r      -> r |> mape document.createElement_h1              "H1"       |> o
-            | Div r     -> r |> mape document.createElement_div             "DIV"      |> o
-            | Text r    -> r |> mapc (fun () -> document.createTextNode "") "#text"    |> o
-            | Comment r -> r |> mapc (fun () -> document.createComment "")  "#comment" |> o
-            | Patch r   -> r |> most.map (fun patch (n, _) -> patch n)                 |> o
-            | _         -> ()
+            | A r           -> r |> mape document.createElement_a               "A"           |> o
+            | H1 r          -> r |> mape document.createElement_h1              "H1"          |> o
+            | Div r         -> r |> mape document.createElement_div             "DIV"         |> o
+            | Custom (t, r) -> r |> mape (fun () -> document.createElement t)   (t.ToUpper()) |> o
+            | Text r        -> r |> mapc (fun () -> document.createTextNode "") "#text"       |> o
+            | Comment r     -> r |> mapc (fun () -> document.createComment "")  "#comment"    |> o
+            | Patch r       -> r |> most.map (fun patch (n, _) -> patch n)                    |> o
+            | _             -> ()
         pith ray
         o (most.now (fun (n, index) -> 
             let childNodes = n.childNodes
--- core/new2.fs
+++ core/new2.fs
@@ -4,6 +4,7 @@ open Fable.Import.Browser
 open M
 open Dom
 open Fable.Core
+open Fable.Core.JsInterop
 let t f = tree (most.periodic 1000 |> most.constant f)
 
 [<Emit("((f)=>{var b;return(a)=>{if(f){b=f(a);f=null;}return b;};})($0)")>]
@@ -12,6 +13,7 @@ let once (_: 'a -> 'b): 'a -> 'b = jsNative
 let rez: R<HTMLElement> = t (fun o ->
     let once f = most.now (once f)
     (H1 (once (fun x -> x.innerText <- "Hello World!1";())), Some "as1") |> o
+    (Custom ("h2", once (fun x -> x.innerText <- "Hello World!1";())), Some "as1") |> o
     (Div (t (fun o ->
         (H1 (once (fun x -> x.innerText <- "Hello World!2";())), Some "as2") |> o
         (Div (once (fun x -> x.innerText <- "Hello World!3";())), Some "as3") |> o
@@ -22,19 +24,3 @@ let rootNode = document.getElementById "root-node"
 let patches = rez |> most.scan (fun n p -> p(n); n) rootNode |> most.skip 1 |> most.take 3
 
 most.runEffects patches (Most.Scheduler.require.newDefaultScheduler ()) |> ignore
-open Fable.Core.JsInterop
-
-type Props =
-| A of int
-| B of string
-| C
-let m:int = !! most
-
-let mk = keyValueList CaseRules.None
-let lst = [
-    A 1
-    B "2"
-    C
-]
-let obj = mk lst
-obj |> console.log
