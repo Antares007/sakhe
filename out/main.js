52efd4cd471752b8ef236dea51f02aae7ce78a8c
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-09 12:10:35 +0400

    fix browserwin size

--- out/main.js
+++ out/main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1280, height: 1024})
+  mainWindow = new BrowserWindow({width: 1280, height: 800})
   mainWindow.webContents.openDevTools()
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
