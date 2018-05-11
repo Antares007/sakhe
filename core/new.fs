1997d5129a742f1ca4244f61ad38b71689be278a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-11 22:43:32 +0400

    corrects generalization issue

--- core/new.fs
+++ core/new.fs
@@ -13,19 +13,21 @@ type Patch<'a> =
 
 
 type ILang<'A when 'A :> Element> =
+    abstract A: pith: Stream<ILang<HTMLAnchorElement> -> unit> * ?key: string -> unit
     abstract Div: pith: Stream<ILang<HTMLDivElement> -> unit> * ?key: string -> unit
     abstract CharData<'C when 'C :> CharacterData> : (unit -> 'C) * (Node -> 'C option) * Stream<'C -> unit> -> unit
     abstract Patch: Stream<'A -> unit> -> unit
 
-#nowarn "64"
+// #nowarn "64"
 
-let rec tree (pith: Stream<ILang<'A> -> unit>): Stream<'A -> unit> =
+let rec tree<'A when 'A :> Element> (pith: Stream<ILang<'A> -> unit>): Stream<'A -> unit> =
     let ring (pith: ILang<'A> -> unit): Pith<Stream<'A -> unit>> =
         fun o ->
             let mutable c = 0
             let cpp a = c <- c + 1; a
             pith { new ILang<'A> with
 
+                member __.A (pith, key) = tree pith |> mape (document.createElement_a,             "A",         key, c) |> cpp |> o
                 member __.Div (pith, key) = tree pith |> mape (document.createElement_div,             "DIV",         key, c) |> cpp |> o
                 member __.CharData (absurd, eq, s) = s |> most.map (Patch.mkPatcher c (absurd, eq)) |> o
                 member __.Patch (s) = s |> most.map (fun patch n -> patch n) |> o }
