775f4347f24b19ac0c15cd2ccd8eb939c4865c51
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-12 17:36:37 +0400

    extend sample code

--- core/new2.fs
+++ core/new2.fs
@@ -4,21 +4,37 @@ open M
 open Dom
 open Most
 
-let a = (document.createElement_a, fun _ -> None)
-let div = (document.createElement_div, fun _ -> None)
-let text = ((fun () -> document.createTextNode ("")), fun _ -> None)
 
-let rez: Stream<HTMLElement -> unit> = tree (most.now (fun o ->
-    o.Node (div, most.now (fun o ->
-        o.Leaf (text, most.now (fun o -> o.textContent <- "hello world!"))
-        o.Node (div, most.now (fun o ->
-            o.Leaf (text, most.now (fun o -> o.textContent <- "hello world!!"))
-            o.Node (div, most.now (fun o ->
-                o.Leaf (text, most.now (fun o -> o.textContent <- "hello world!!!"))
+
+
+let mkTypeChecker (create: unit -> 't) (prove: _ -> bool): (unit -> 't) * (Node -> 't option) =
+    (create, fun n -> if prove n then Some (unbox n) else None)
+let inline NodeName nodeName (node: #Node) = node.nodeName = nodeName
+
+
+let a = mkTypeChecker document.createElement_a (NodeName "A")
+let h1 = mkTypeChecker document.createElement_h1 (NodeName "H1")
+let h2 = mkTypeChecker document.createElement_h1 (NodeName "H2")
+let div = mkTypeChecker document.createElement_div (NodeName "DIV")
+let button = mkTypeChecker document.createElement_button (NodeName "BUTTON")
+let text = mkTypeChecker (fun () -> document.createTextNode ("")) (NodeName "#text")
+
+let (!^) x = most.now x
+let (<|>) a b = a (most.now b)
+
+let rez: Stream<HTMLElement -> unit> = tree <|> fun o ->
+    o.Node (div, !^ (fun o ->
+        o.Leaf (text, !^ (fun o -> o.textContent <- "a"))
+        o.Node (div, !^ (fun o ->
+            o.Leaf (text, !^ (fun o -> o.textContent <- "b"))
+            o.Node (h1, !^ (fun o ->
+                o.Leaf (text, !^ (fun o -> o.textContent <- "c"))
             ))
         ))
     ))
-))
+    o.Node (button, !^ (fun o ->
+        o.Leaf (text, !^ (fun o -> o.textContent <- "click me"))
+    ))
 
 let rootNode = document.getElementById "root-node"
 let patches = rez |> most.scan (fun n p -> p(n); n) rootNode |> most.skip 1 |> most.take 3
