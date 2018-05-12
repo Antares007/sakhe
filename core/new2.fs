6879fe8779982de7b6d035d42dbfdc33fd9ec586
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-12 21:46:18 +0400

    extend sample code with composition

--- core/new2.fs
+++ core/new2.fs
@@ -10,6 +10,7 @@ let mkAbsurdProve (create: unit -> 't) (prove: _ -> bool): (unit -> 't) * (Node 
 let inline NodeName nodeName (node: #Node) = node.nodeName = nodeName
 
 let elm (ap: AbsurdProve<'a>) (pith: Stream<ILang<'a> -> unit>) = (ap, pith)
+let chr (ap: AbsurdProve<'a>) (pith: Stream<'a -> unit>) = (ap, pith)
 
 let a = mkAbsurdProve document.createElement_a (NodeName "A")
 let h1 = mkAbsurdProve document.createElement_h1 (NodeName "H1")
@@ -18,12 +19,21 @@ let div = mkAbsurdProve document.createElement_div (NodeName "DIV")
 let button = mkAbsurdProve document.createElement_button (NodeName "BUTTON")
 let text = mkAbsurdProve (fun () -> document.createTextNode ("")) (NodeName "#text")
 
+let Div f = elm div (most.now f)
 let Button f = elm button (most.now f)
+let Text f = chr text f
 
 let (!^) x = most.now x
 let (<|>) a b = a (most.now b)
+let (:=) a b = a (most.now b)
 
-let rez: Stream<HTMLElement -> unit> = tree <|> fun o ->
+let rez: Stream<HTMLElement -> unit> = tree := fun o ->
+    o.Node << Div <| fun o ->
+        o.Node << Div <| fun o ->
+            o.Node << Div <| fun o ->
+                o.Node << Div <| fun o ->
+                    o.Leaf << Text := fun text -> text.textContent <- "deeep text"
+
     o.Node (div, !^ (fun o ->
         o.Leaf (text, !^ (fun o -> o.textContent <- "a"))
         o.Node (div, !^ (fun o ->
