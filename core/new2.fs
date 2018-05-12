987c7fde40355c5adc1ae6b9f2ab5db249310db6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-12 20:01:46 +0400

    add sample code

--- core/new2.fs
+++ core/new2.fs
@@ -3,21 +3,22 @@ open Fable.Import.Browser
 open M
 open Dom
 open Most
+open Patch
 
-
-
-
-let mkTypeChecker (create: unit -> 't) (prove: _ -> bool): (unit -> 't) * (Node -> 't option) =
+let mkAbsurdProve (create: unit -> 't) (prove: _ -> bool): (unit -> 't) * (Node -> 't option) =
     (create, fun n -> if prove n then Some (unbox n) else None)
 let inline NodeName nodeName (node: #Node) = node.nodeName = nodeName
 
+let elm (ap: AbsurdProve<'a>) (pith: Stream<ILang<'a> -> unit>) = (ap, pith)
 
-let a = mkTypeChecker document.createElement_a (NodeName "A")
-let h1 = mkTypeChecker document.createElement_h1 (NodeName "H1")
-let h2 = mkTypeChecker document.createElement_h1 (NodeName "H2")
-let div = mkTypeChecker document.createElement_div (NodeName "DIV")
-let button = mkTypeChecker document.createElement_button (NodeName "BUTTON")
-let text = mkTypeChecker (fun () -> document.createTextNode ("")) (NodeName "#text")
+let a = mkAbsurdProve document.createElement_a (NodeName "A")
+let h1 = mkAbsurdProve document.createElement_h1 (NodeName "H1")
+let h2 = mkAbsurdProve document.createElement_h1 (NodeName "H2")
+let div = mkAbsurdProve document.createElement_div (NodeName "DIV")
+let button = mkAbsurdProve document.createElement_button (NodeName "BUTTON")
+let text = mkAbsurdProve (fun () -> document.createTextNode ("")) (NodeName "#text")
+
+let Button f = elm button (most.now f)
 
 let (!^) x = most.now x
 let (<|>) a b = a (most.now b)
@@ -31,6 +32,12 @@ let rez: Stream<HTMLElement -> unit> = tree <|> fun o ->
                 o.Leaf (text, !^ (fun o -> o.textContent <- "c"))
             ))
         ))
+    ))
+    o.Node (Button <| fun o ->
+        o.Leaf (text, !^ (fun o -> o.textContent <- "hmmmm"))
+    )
+    o.Node (button, !^ (fun o ->
+        o.Leaf (text, !^ (fun o -> o.textContent <- "click me"))
     ))
     o.Node (button, !^ (fun o ->
         o.Leaf (text, !^ (fun o -> o.textContent <- "click me"))
