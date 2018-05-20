e5d55562ab16529557105c49e562a0d22fad94e8
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-20 13:01:31 +0400

    sync: add impl to otree

--- core/state.fs
+++ core/state.fs
@@ -24,15 +24,31 @@ and IArray =
     abstract Value<'a> : (unit -> 'a) * (obj -> bool) -> r: IStream<'a -> 'a> -> unit
 open Dom2
 open Fable.Import.Browser
+open Fable.Core.JsInterop
 console.log run
+
+let private setKey (_: string) (_: 'a) (_: 'b): 'b = jsNative
+
 let otree
     (pith: IStream<IObject -> unit>): IStream<obj -> obj> =
     let ring pith o =
+        let mutable rMap = Map.empty<string, IStream<obj -> obj>>
         pith { new IObject with
         member __.ONode key r = failwith "ni"
         member __.ANode key r = failwith "ni"
-        member __.Value key (absurd, pove) r = failwith "ni"
+        member __.Value key (absurd, prove) r =
+            let r =
+                M.map
+                    (fun r o ->
+                        let x = o?key
+                        let oa = if prove x then (unbox x) else absurd ()
+                        let na = r oa
+                        setKey key na o
+                    )
+                    r
+            rMap <- Map.add key r rMap
         }
+        Map.iter (fun _ r -> o r) rMap
 
     let deltac xs =
         failwith "ni"
