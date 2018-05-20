9bec73e96a11d9ea329f1515d235943d209b53d3
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-05-20 10:06:09 +0400

    sync

--- core/state.fs
+++ core/state.fs
@@ -9,14 +9,36 @@ module Sakhe.State
 //     | JArray  of JValue list
 
 open Fable.Core
+open Most
+type T<'a> = (unit -> 'a) * (obj -> bool)
 
 type IObject =
-    abstract ONode: int -> unit
-    abstract ANode: int -> unit
-    abstract Value: int -> unit
+    abstract ONode: key: string -> (IObject -> unit) -> unit
+    abstract ANode: key: string -> (IArray -> unit) -> unit
+    abstract Value<'a> : key: string -> (unit -> 'a) * (obj -> bool) * IStream<'a -> 'a> -> unit
+and IArray =
+    abstract ONode: (IObject -> unit) -> unit
+    abstract ANode: (IArray -> unit) -> unit
+    abstract Value<'a> : t: T<'a> * r: IStream<'a -> 'a> -> unit
+
 
 type [<Pojo>] Person =
     {name: string; age: float}
+    static member Absurd () = {name = "a"; age = 42.}
+
+let person = {name = "Archil"; age = 42.}
+let zero = Person.Absurd ()
+
+let o: IObject = unbox false
+
+o.Value
+    "a"
+    (
+        (fun () -> {name="a"; age = 42.}),
+        (fun o -> true),
+        (M.now (fun x -> x))
+    )
+
 
 let obj: obj =
     Fable.Core.JsInterop.createEmpty
