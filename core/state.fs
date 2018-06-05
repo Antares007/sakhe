4bdaa556f66240de3f62f13a7ad233016c1f6224
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-06-05 21:31:22 +0400

    clean

--- core/state.fs
+++ core/state.fs
@@ -1,22 +1,16 @@
 module Sakhe.State
 open Update
-/// Wraps a state of type 'T
-type StateState<'T> = State of 'T
 
-/// Represents updates on state of type 'T
+type StateState<'T> = State of 'T
 type StateUpdate<'T> =
     | Set of 'T
     | SetNop
-
-    /// Empty update - do not change the state
-    static member Unit:StateUpdate<'T> = SetNop
-    /// Combine updates - return the latest (rightmost) 'Set' update
+    static member Unit: StateUpdate<'T> = SetNop
     static member Combine(a, b) =
         match a, b with
         | SetNop, v -> v
         | v, SetNop -> v
         | Set _, Set b -> Set b
-    /// Apply update to a state - the 'Set' update changes the state
     static member Apply(s, p) =
         match p with
         | SetNop -> s
