d2257c7fce79b009f7517b91ed58a60b977cec44
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-06-08 14:42:37 +0400

    pith

--- core/pith.fs
+++ core/pith.fs
@@ -4,9 +4,44 @@ open Fable.Core
 [<Erase>] type Pith<'a> = Pith of (('a -> unit) -> unit)
 
 module Pith =
-    let map f (Pith p) = Pith <| fun o -> p (o << f)
+
+    let return' p =
+        Pith p
+
+    let map f (Pith p) =
+        Pith <| fun o -> p (o << f)
+
+    let bind f (Pith pa) =
+        Pith <| fun ob ->
+            pa <| fun oa ->
+                let (Pith pb) = f oa
+                pb ob
+
+    let fish f g a =
+        Pith <| fun oc ->
+            let (Pith pithB) = f a
+            pithB <| fun b ->
+                let (Pith pithC) = g b
+                pithC oc
 
     let tree deltaC (Pith pith) =
         let mutable list = []
         pith (fun a -> list <- a :: list)
         deltaC list
+
+    let see =
+        return' <| fun o ->
+            o 0
+            o 1
+            o 2
+        |> map ((+) 1)
+        |> (
+            bind <| fun a ->
+                return' <| fun o ->
+                    o <| "A" + string a
+                    o <| "B" + string a
+                    o <| "O" + string a
+        )
+        |> tree (List.fold (+) "")
+
+    printfn "rez: %A" see
--- out/modules/pith.js
+++ out/modules/pith.js
@@ -9,12 +9,43 @@ var _List = require("./fable-core/List");
 
 var _List2 = _interopRequireDefault(_List);
 
+var _Seq = require("./fable-core/Seq");
+
+var _CurriedLambda = require("./fable-core/CurriedLambda");
+
+var _CurriedLambda2 = _interopRequireDefault(_CurriedLambda);
+
+var _String = require("./fable-core/String");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Pith = exports.Pith = function (__exports) {
+  const return_ = __exports["return'"] = function (p) {
+    return p;
+  };
+
   const map = __exports.map = function (f, _arg1) {
     return function (o) {
       _arg1($var1 => o(f($var1)));
+    };
+  };
+
+  const bind = __exports.bind = function (f, _arg1) {
+    return function (ob) {
+      _arg1(function (oa) {
+        const patternInput = f(oa);
+        patternInput(ob);
+      });
+    };
+  };
+
+  const fish = __exports.fish = function (f, g, a) {
+    return function (oc) {
+      const patternInput = f(a);
+      patternInput(function (b) {
+        const patternInput_1 = g(b);
+        patternInput_1(oc);
+      });
     };
   };
 
@@ -28,5 +59,27 @@ const Pith = exports.Pith = function (__exports) {
     return deltaC(list);
   };
 
+  const see = __exports.see = tree((() => {
+    const folder = function (x, y) {
+      return x + y;
+    };
+
+    return function (list) {
+      return (0, _Seq.fold)(folder, "", list);
+    };
+  })(), (0, _CurriedLambda2.default)(bind)(function (a) {
+    return return_(function (o) {
+      o("A" + a.toString());
+      o("B" + a.toString());
+      o("O" + a.toString());
+    });
+  })(map(function (y_1) {
+    return 1 + y_1;
+  }, return_(function (o_1) {
+    o_1(0);
+    o_1(1);
+    o_1(2);
+  }))));
+  (0, _String.toConsole)((0, _String.printf)("rez: %A"))(see);
   return __exports;
 }({});
