89f256aa3d4cc31e41412c51c9e28f58fad465e4
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-06-10 00:39:38 +0400

    clean p

--- core/p.fs
+++ core/p.fs
@@ -3,29 +3,6 @@ open Fable.Core
 
 [<Erase>] type P<'a> = private P of ('a -> unit)
 
-module State =
-
-    type S<'State,'Value> =
-        S of ('State -> 'Value * 'State)
-
-    let runS (S f) state = f state
-
-    let returnS x =
-        let run state =
-            x, state
-        S run
-
-    let bindS f xS =
-        let run state =
-            let x, newState = runS xS state
-            runS (f x) newState
-        S run
-
-    let xS = returnS (P <| fun (_:int) -> ())
-    let see = runS xS "1"
-    // let tree s pith =
-    //     S.treeCombine (fun a b -> returnS (a, b)) s pith
-
 module P =
     open System.Collections.Generic
 
@@ -49,8 +26,4 @@ module P =
     let apply n (P p) = p n |> ignore; n
 
     let tree s pith =
-        // let cmb = S.combine combine
-        // let ring pith = A.tree cmb s pith
-        // S.switchLatest (S.map ring pith)
-        // S.tree (S.combine combine) (S.now s) pith
         S.treeCombine combine s pith
