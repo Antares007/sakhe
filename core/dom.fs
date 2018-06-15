8e647d11c1940f94b3823b87d42565b4e4fc9df7
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-06-15 19:51:39 +0400

    make ring with pmap

--- core/dom.fs
+++ core/dom.fs
@@ -45,18 +45,23 @@ type ATree =
     | Text of update: S<Update.M<AText.S, AText.U, unit>>
 
 let private update = Update.update
+let private addIndex f =
+    let mutable i = 0
+    fun a ->
+        let index = i
+        i <- i + 1
+        f (a, index)
 
 let tree f s (p: S<Pith<ATree>>): S<Update.M<AElement.S, AElement.U, unit>> =
-    let ring (Pith pith) =
-        Pith <| fun o ->
-            pith <| function
-                | Element (tag, key, su) ->
-                    su |> S.map (fun x -> update {
-                        let! y = x
-                        return 0
-                    }) |> o
-
-                | Text _ -> ()
-
-
-    S.treeCombine f s (S.map ring p)
+    let ring = S.map << Pith.map << addIndex <| function
+        | Element (tag, key, su), i ->
+            su |> S.map (fun x -> update {
+                let! y = x
+                return "0"
+            })
+        | Text su, i ->
+            su |> S.map (fun x -> update {
+                // let! y = x
+                return "0"
+            })
+    S.treeCombine f s (ring p)
