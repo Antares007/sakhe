0761f225fb1f3abafa8b3f3f2f36e35ea6079074
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-06-19 14:09:00 +0400

    sync dom, redefine pith

--- core/dom.fs
+++ core/dom.fs
@@ -1,14 +1,51 @@
 module Sakhe.Dom
-open Fable.Core
 
-type VText = {
+
+type IO<'T> = private | Action of (unit -> 'T)
+
+[<AutoOpen>]
+module MonadIO =
+    let private raw  (Action f) = f
+    let private run  io         = raw io ()
+    let private eff  g   io     = raw io () |> g
+    let private bind io  rest   = Action (fun () -> io |> eff rest |> run)
+    let private comb io1 io2    = Action (fun () -> run io1; run io2)
+
+    type IOBuilder() =
+        member __.Return(x)              = Action (fun () -> x)
+        member __.ReturnFrom(io) : IO<_> = io
+        member __.Delay(g) : IO<_>       = g ()
+        member __.Bind(io, rest)         = bind io rest
+        member __.Combine(io1, io2)      = comb io1 io2
+
+    let io = new IOBuilder()
+    // let (|Action|) io = run io
+
+[<AutoOpen>]
+module PreludeIO =
+    let putChar  (c:char)   = Action (fun () -> stdout.Write(c))
+    let putStr   (s:string) = Action (fun () -> stdout.Write(s))
+    let putStrLn (s:string) = Action (fun () -> stdout.WriteLine(s))
+    let print x             = Action (fun () -> printfn "%A" x)
+    let getChar     = Action (fun () -> stdin.Read() |> char |> string)
+    let getLine     = Action (fun () -> stdin.ReadLine())
+    let getContents = Action (fun () -> stdin.ReadToEnd())
+
+type VTree =
+    | VElement of VElement
+    | VText of VText
+and VText = {
     data: string
     node: Fable.Import.Browser.Text
     }
-type VElement = {
+and VElement = {
     tag: string
     data: obj
-    children: U2<VElement, VText> []
+    // indexElementMap: Map<int, VElement>
+    // indexTextMap: Map<int, VText>
+    // keyElementMap: Map<string, int>
+    // lenght: int
+    children: VTree []
     node: Fable.Import.Browser.Element
     }
 
@@ -17,34 +54,72 @@ type ATree =
     | Text of update: S<State.M<VText, unit>>
 
 let private state = State.state
-let private addIndex f =
-    let mutable i = 0
-    fun a ->
-        let index = i
-        i <- i + 1
-        f index a
+let rec private tryFind prove (i: int) (children: VTree []) =
+    if i >= children.Length then
+        None
+    else
+        let n = children.[i]
+        if prove n then
+            Some n
+        else
+            tryFind prove (i + 1) children
+let inline private matchChildren
+    (
+        notFound: unit -> unit,
+        atIndex: VTree -> unit,
+        movedFrom: VTree * VTree -> unit
+    )
+    (prove: VTree -> bool, index: int, children: VTree []) =
+    if index >= children.Length then
+        notFound ()
+    else
+        let childAtIndex = children.[index]
+        if prove childAtIndex then
+            atIndex childAtIndex
+        else
+            match tryFind prove (index + 1) children with
+            | Some moved -> movedFrom (moved, childAtIndex)
+            | None -> notFound ()
 
-let tree s (p: S<Pith<ATree>>): S<State.M<VElement, unit>> =
-    let ring = S.map << Pith.map << addIndex <| fun index -> function
-        | Element (tag, key, su) ->
-            su |> S.map (fun x -> state {
-                let! p = State.get
-                do! (State.set p)
-                // let z = match p.children.[index] with
-                //     | U2.Case1 (x) -> 1
-                //     | U2.Case2 (x) -> 2
-                // let! y = x
-                return ()
-            })
-        | Text su ->
-            su |> S.map (fun x -> state {
-                let! p = State.get
-                // let! y = x
-                // let rez = AText.setRun {data = ""; node = Fable.Import.Browser.document.createTextNode ""}
-                return ()
-            })
+
+let tree f s (p: S<Pith<ATree>>) =
+    let ring (Pith pith): Pith<S<State.M<VElement, unit>>> =
+        Pith <| fun o ->
+            let mutable i = 0
+            pith <| function
+                | Element (tag, key, su) ->
+                    let index = i
+                    i <- i + 1
+                    o << S.map (fun x -> state {
+                        let! p = State.get
+                        matchChildren
+                            (
+                                (fun () -> ()),
+                                (function
+                                    | VText _ -> ()
+                                    | VElement e -> ()
+                                ),
+                                (fun (vtree, vtreeRef) -> ())
+                            )
+                            (
+                                (fun vtree -> false),
+                                index,
+                                p.children
+                            )
+                        return ()
+                    }) <| su
+                | Text su ->
+                    let index = i
+                    i <- i + 1
+                    o << S.map (fun x -> state {
+                        let! p = State.get
+                        // let! y = x
+                        // let rez = AText.setRun {data = ""; node = Fable.Import.Browser.document.createTextNode ""}
+                        return ()
+                    }) <| su
+
     S.treeCombine (fun s1 s2 -> state {
         let! a = s1
         let! b = s2
-        return ()
-    }) s (ring p)
+        return f a b
+    }) s (S.map ring p)
--- core/pith.fs
+++ core/pith.fs
@@ -4,11 +4,6 @@ open Fable.Core
 [<Erase>] type Pith<'a> = Pith of (('a -> unit) -> unit)
 
 module Pith =
-    let inline unit< ^a when ^a : (static member Unit : ^a)> () : ^a =
-      (^a : (static member Unit : ^a) ())
-    let inline combine< ^a when ^a: (static member Combine : ^a * ^a -> ^a )> l r =
-      (^a : (static member Combine : ^a * ^a -> ^a) (l, r))
-
     let return' p =
         Pith p
 
@@ -20,16 +15,8 @@ module Pith =
         pith <| fun a -> list <- a :: list
         list
 
-    let inline run p = toList p |> List.fold combine (unit ())
-    let inline run2 (Pith pith) =
-        let mutable a = unit ()
-        pith <| fun a' -> a <- combine a a'
-        a
-
-    let inline run3 combine unit (Pith pith) =
-        let mutable a = unit
-        pith <| fun b -> a <- combine a b
-        a
+    let inline run< ^a when ^a: (static member Tree : Pith< ^a> -> ^a )> p =
+      (^a : (static member Tree : Pith< ^a> -> ^a ) p)
 
     let inline bind f pa =
         Pith <| fun ob ->
@@ -47,17 +34,19 @@ module Pith =
         member __.ReturnFrom(a): Pith<'a> = a
         member inline __.Bind(m, f) = bind f m
     let p =  PithBuilder()
-
-(*     type T =
+(*
+    type T =
         | T of int
-        static member Unit: T = T 0
-        static member Combine(T a, T b) = T (a + b)
-
+        static member Tree(Pith p) =
+            let mutable a = 0
+            p <| fun (T b) -> a <- a + b
+            T a
     type S =
         | S of string
-        static member Unit: S = S ""
-        static member Combine(S a, S b) = S (a + b)
-
+        static member Tree(Pith p) =
+            let mutable a = ""
+            p <| fun (S b) -> a <- a + b
+            S a
     let p0 = p { return fun o ->
         o <| T 1
         o <| T 2
@@ -75,5 +64,5 @@ module Pith =
             o << S  <| "S" + (string v)
     }
 
-    printfn "%A" <| run2 p1
+    printfn "AI: %A" <| run p1
  *)
