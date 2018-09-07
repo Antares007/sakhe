fe32e886be768f09961009722cfa71a9331407c4
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-09-07 15:46:44 +0400

    sync

--- core/s/timeline.fs
+++ core/s/timeline.fs
@@ -1,5 +1,6 @@
 namespace Sakhe.S
 
+open System.Collections.Generic
 [<AutoOpen>]
 module GenericListExtensions =
     open Fable.Core
@@ -19,41 +20,103 @@ module GenericListExtensions =
         member __.``splice`` ((_: int), (_: int), (_: 'a)): ResizeArray<'a> = jsNative
 
 module Timeline =
-    type Time = Time.T
 
-    type T = private Timeline of ResizeArray<Time * Task.T<unit>>
+    type IdRef = private Ref of int ref
 
-    let private findAppendPosition (time: Time) (a: ResizeArray<Time * 'a>) =
-        let rec go l r =
-            if l < r then
-                let m = (l + r) / 2
-                if fst a.[m] > time then go l       m
-                else                     go (m + 1) r
-            else l - 1
-        go 0 (a.length)
+    type SortedArray<'a> = private SortedArray of List<'a>
+    type Slot<'a, 'b> = private Slot of SortedArray<'a> * Dictionary<'a, 'b>
 
-    let empty () = Timeline <| ResizeArray()
+    type T = private Timeline of Slot<Time.T, Slot<int, Task.T<unit>>> * int ref
 
-    let nextArrival (Timeline timeline) =
-        if timeline.length = 0 then Time.return' infinity else fst timeline.[0]
+    module SortedArray =
+        let empty () = SortedArray <| ResizeArray()
 
-    let add time task (Timeline timeline) =
-        let i = findAppendPosition time timeline
-        if i = -1 then timeline.splice (0, 0, (time, task)) |> ignore
+        let init a =
+            let arr = ResizeArray()
+            arr.Add a
+            SortedArray arr
+
+        let inline findAppendPosition a (SortedArray array) =
+            let rec go l r =
+                if l < r then
+                    let m = (l + r) / 2
+                    if array.[m] > a then go l       m
+                    else                  go (m + 1) r
+                else l - 1
+            go 0 (array.length)
+
+        let inline readIndex i (SortedArray arr) =
+            assert (0 <= i && i < arr.length)
+            arr.[i]
+
+        let inline splice start ``end`` (SortedArray array) =
+            array.slice (start, ``end``)
+
+        let inline append a (SortedArray arr) =
+            assert (0 = arr.length || arr.[arr.length - 1] <= a)
+            arr.Add(a)
+
+        let inline insertAfter i a  (SortedArray arr) =
+            assert (-1 <= i            && i < arr.length)
+            assert (i = -1             || arr.[i]     <= a)
+            assert (i = arr.length - 1 || arr.[i + 1] >  a)
+            arr.splice (i + 1, 0, a) |> ignore
+
+    module Slot =
+        let inline empty () = Slot <| (SortedArray.empty(), Dictionary())
+
+        let inline length (Slot (SortedArray array, _)) = array.length
+
+        let inline findAppendPosition a (Slot (array, _)) = SortedArray.findAppendPosition a array
+
+        let inline insertAfter i (a, b) (Slot (array, map)) =
+            SortedArray.insertAfter i a array
+            map.Add(a, b)
+
+        let inline readIndex i (Slot (arr, map)) =
+            let b = SortedArray.readIndex i arr
+            (b, map.GetValueOrDefault b)
+
+        let inline append (id, task) (Slot (ids, taskMap)) =
+            SortedArray.append id ids
+            taskMap.Add(id, task)
+
+        let inline remove id (Slot (ids, taskMap)) =
+            let index = SortedArray.findAppendPosition id ids
+            assert (index <> -1 && id = SortedArray.readIndex index ids)
+            taskMap.Remove(id) |> ignore
+            SortedArray.splice index 1
+
+    let empty () = Timeline <| (Slot.empty (), ref 0)
+
+    let nextArrival (Timeline (slot, _)) =
+        if Slot.length slot = 0 then Time.return' infinity else (fst (Slot.readIndex 0 slot))
+
+    let add (time: Time.T) task (Timeline (tslot, id)) =
+        let i = Slot.findAppendPosition time tslot
+        let insertAfter i =
+            let slot = Slot.empty ()
+            Slot.append (id.Value, task) slot
+            Slot.insertAfter i (time, (slot)) tslot
+        if i = -1 then
+            insertAfter i
         else
-        let (iTime, iTask) = timeline.[i]
-        if iTime < time then timeline.[i] <- (time, Task.append iTask task)
-        else timeline.splice (i + 1, 0, (time, task)) |> ignore
-
-    let removeTasks time (Timeline timeline) =
-        let tasks = timeline.splice (0, findAppendPosition time timeline + 1)
-        let length = tasks.length
-        if length = 0 then None
-        else
-            let (_, firstTask) = tasks.[0]
-            if length = 1 then Some firstTask
+            let (itime, slot) = Slot.readIndex i tslot
+            if itime = time then
+                Slot.append (id.Value, task) slot
             else
-            let rec go i acc =
-                if i >= length then acc
-                else go (i + 1) (Task.append acc (snd timeline.[i]))
-            Some <| go 1 firstTask
+                insertAfter i
+        id.Value <- id.Value + 1
+
+    // let removeTasks time (Timeline timeline) =
+    //     let tasks = timeline.splice (0, findAppendPosition time timeline + 1)
+    //     let length = tasks.length
+    //     if length = 0 then None
+    //     else
+    //         let (_, firstTask) = tasks.[0]
+    //         if length = 1 then Some firstTask
+    //         else
+    //         let rec go i acc =
+    //             if i >= length then acc
+    //             else go (i + 1) (Task.append acc (snd timeline.[i]))
+    //         Some <| go 1 firstTask
--- out/modules/s/timeline.js
+++ out/modules/s/timeline.js
@@ -1,17 +1,61 @@
-import { declare, Union } from "../fable-core.2.0.0-beta-003/Types";
-import { compare } from "../fable-core.2.0.0-beta-003/Util";
-import { append } from "./task";
+import { FSharpRef, declare, Union } from "../fable-core.2.0.0-beta-003/Types";
+import { equals, compare } from "../fable-core.2.0.0-beta-003/Util";
+export const Timeline$002EIdRef = declare(function Timeline$002EIdRef(tag, name, ...fields) {
+  Union.call(this, tag, name, ...fields);
+}, Union);
+export const Timeline$002ESortedArray$00601 = declare(function Timeline$002ESortedArray$00601(tag, name, ...fields) {
+  Union.call(this, tag, name, ...fields);
+}, Union);
+export const Timeline$002ESlot$00602 = declare(function Timeline$002ESlot$00602(tag, name, ...fields) {
+  Union.call(this, tag, name, ...fields);
+}, Union);
 export const Timeline$002ET = declare(function Timeline$002ET(tag, name, ...fields) {
   Union.call(this, tag, name, ...fields);
 }, Union);
+export function Timeline$002ESortedArray$$$empty() {
+  return new Timeline$002ESortedArray$00601(0, "SortedArray", []);
+}
+export function Timeline$002ESortedArray$$$init(a) {
+  const arr = [];
+  arr.push(a);
+  return new Timeline$002ESortedArray$00601(0, "SortedArray", arr);
+}
+export function Timeline$$$empty() {
+  var tupledArg$$1;
+  const tupledArg = [(tupledArg$$1 = [Timeline$002ESortedArray$$$empty(), new Map([])], new Timeline$002ESlot$00602(0, "Slot", tupledArg$$1[0], tupledArg$$1[1])), new FSharpRef(0)];
+  return new Timeline$002ET(0, "Timeline", tupledArg[0], tupledArg[1]);
+}
+export function Timeline$$$nextArrival(_arg1) {
+  var array, map, arr$$1, b, arr$$2;
+  const slot = _arg1.fields[0];
 
-function Timeline$$$findAppendPosition(time, a) {
+  if ((array = slot.fields[0].fields[0], array.length) === 0) {
+    const f = Number.POSITIVE_INFINITY;
+
+    if (!(f >= 0)) {
+      debugger;
+    }
+
+    return Math.floor(f);
+  } else {
+    return (map = slot.fields[1], (arr$$1 = slot.fields[0], (b = (arr$$2 = arr$$1.fields[0], (!(0 <= 0 ? 0 < arr$$2.length : false) ? (() => {
+      debugger;
+    })() : null, arr$$2[0])), [b, null])))[0];
+  }
+}
+export function Timeline$$$add(time, task, _arg1$$4) {
+  const tslot = _arg1$$4.fields[0];
+  const id = _arg1$$4.fields[1];
+  let i$$2;
+  const array$$1 = tslot.fields[0];
+  const array$$2 = array$$1.fields[0];
+
   const go = function go(l, r) {
     go: while (true) {
       if (l < r) {
         const m = ~~((l + r) / 2) | 0;
 
-        if (compare(a[m][0], time) > 0) {
+        if (compare(array$$2[m], time) > 0) {
           l = l;
           r = m;
           continue go;
@@ -26,70 +70,75 @@ function Timeline$$$findAppendPosition(time, a) {
     }
   };
 
-  return go(0, a.length) | 0;
-}
+  i$$2 = go(0, array$$2.length);
 
-export function Timeline$$$empty() {
-  return new Timeline$002ET(0, "Timeline", []);
-}
-export function Timeline$$$nextArrival(_arg1) {
-  const timeline = _arg1.fields[0];
+  const insertAfter = function insertAfter(i$$3) {
+    let slot$$1;
+    const tupledArg$$2 = [Timeline$002ESortedArray$$$empty(), new Map([])];
+    slot$$1 = new Timeline$002ESlot$00602(0, "Slot", tupledArg$$2[0], tupledArg$$2[1]);
+    const id$$1 = id.contents | 0;
+    const taskMap = slot$$1.fields[1];
+    const ids = slot$$1.fields[0];
+    const arr$$3 = ids.fields[0];
 
-  if (timeline.length === 0) {
-    const f = Number.POSITIVE_INFINITY;
-
-    if (!(f >= 0)) {
+    if (!(0 === arr$$3.length ? true : arr$$3[arr$$3.length - 1] <= id$$1)) {
       debugger;
     }
 
-    return Math.floor(f);
-  } else {
-    return timeline[0][0];
-  }
-}
-export function Timeline$$$add(time$$1, task, _arg1$$1) {
-  const timeline$$1 = _arg1$$1.fields[0];
-  const i = Timeline$$$findAppendPosition(time$$1, timeline$$1) | 0;
+    arr$$3.push(id$$1);
+    taskMap.set(id$$1, task);
+    const map$$1 = tslot.fields[1];
+    const array$$3 = tslot.fields[0];
+    const arr$$4 = array$$3.fields[0];
 
-  if (i === -1) {
-    timeline$$1.splice(0, 0, [time$$1, task]);
-  } else {
-    const patternInput = timeline$$1[i];
+    if (!(-1 <= i$$3 ? i$$3 < arr$$4.length : false)) {
+      debugger;
+    }
 
-    if (compare(patternInput[0], time$$1) < 0) {
-      timeline$$1[i] = [time$$1, append(patternInput[1], task)];
+    if (!(i$$3 === -1 ? true : compare(arr$$4[i$$3], time) <= 0)) {
+      debugger;
+    }
+
+    if (!(i$$3 === arr$$4.length - 1 ? true : compare(arr$$4[i$$3 + 1], time) > 0)) {
+      debugger;
+    }
+
+    arr$$4.splice(i$$3 + 1, 0, time);
+    map$$1.set(time, slot$$1);
+  };
+
+  if (i$$2 === -1) {
+    insertAfter(i$$2);
+  } else {
+    let patternInput;
+    const map$$2 = tslot.fields[1];
+    const arr$$5 = tslot.fields[0];
+    let b$$2;
+    const arr$$6 = arr$$5.fields[0];
+
+    if (!(0 <= i$$2 ? i$$2 < arr$$6.length : false)) {
+      debugger;
+    }
+
+    b$$2 = arr$$6[i$$2];
+    patternInput = [b$$2, null];
+
+    if (equals(patternInput[0], time)) {
+      const id$$2 = id.contents | 0;
+      const taskMap$$1 = patternInput[1].fields[1];
+      const ids$$1 = patternInput[1].fields[0];
+      const arr$$7 = ids$$1.fields[0];
+
+      if (!(0 === arr$$7.length ? true : arr$$7[arr$$7.length - 1] <= id$$2)) {
+        debugger;
+      }
+
+      arr$$7.push(id$$2);
+      taskMap$$1.set(id$$2, task);
     } else {
-      timeline$$1.splice(i + 1, 0, [time$$1, task]);
+      insertAfter(i$$2);
     }
   }
-}
-export function Timeline$$$removeTasks(time$$2, _arg1$$2) {
-  const timeline$$2 = _arg1$$2.fields[0];
-  const tasks = timeline$$2.splice(0, Timeline$$$findAppendPosition(time$$2, timeline$$2) + 1);
-  const length = tasks.length | 0;
 
-  if (length === 0) {
-    return null;
-  } else {
-    const patternInput$$1 = tasks[0];
-
-    if (length === 1) {
-      return patternInput$$1[1];
-    } else {
-      const go$$1 = function go$$1(i$$1, acc) {
-        go$$1: while (true) {
-          if (i$$1 >= length) {
-            return acc;
-          } else {
-            const $var$$1 = i$$1 + 1;
-            acc = append(acc, timeline$$2[i$$1][1]);
-            i$$1 = $var$$1;
-            continue go$$1;
-          }
-        }
-      };
-
-      return go$$1(1, patternInput$$1[1]);
-    }
-  }
+  id.contents = id.contents + 1;
 }
