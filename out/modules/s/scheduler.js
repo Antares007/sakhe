26f9f0e5efad2e9d55f8b3a3c3cc77abd705c127
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-09-10 11:58:38 +0400

    move timeline to scheduler private module

--- out/modules/s/scheduler.js
+++ out/modules/s/scheduler.js
@@ -6,15 +6,17 @@ Object.defineProperty(exports, "__esModule", {
 exports.return$0027 = return$0027;
 exports.schedule = schedule;
 
-var _Types = require("../fable-core.2.0.0-beta-004/Types");
-
-var _timeline = require("./timeline");
-
 var _Util = require("../fable-core.2.0.0-beta-004/Util");
 
-var _clock = require("./clock");
-
 var _task = require("./task");
+
+var _Seq = require("../fable-core.2.0.0-beta-004/Seq");
+
+var _Array = require("../fable-core.2.0.0-beta-004/Array");
+
+var _Types = require("../fable-core.2.0.0-beta-004/Types");
+
+var _clock = require("./clock");
 
 var _time = require("./time");
 
@@ -22,14 +24,76 @@ var _timer = require("./timer");
 
 var _disposable = require("./disposable");
 
+function TimelineModule$$$findAppendPosition(a, array) {
+  const go = function go(l, r) {
+    go: while (true) {
+      if (l < r) {
+        const m = ~~((l + r) / 2) | 0;
+
+        if ((0, _Util.compare)(array[m][0], a) > 0) {
+          l = l;
+          r = m;
+          continue go;
+        } else {
+          l = m + 1;
+          r = r;
+          continue go;
+        }
+      } else {
+        return l - 1 | 0;
+      }
+    }
+  };
+
+  return go(0, array.length) | 0;
+}
+
+function TimelineModule$$$empty() {
+  return [];
+}
+
+function TimelineModule$$$nextArrival(_arg1) {
+  const array$$1 = _arg1;
+
+  if (array$$1.length === 0) {
+    return null;
+  } else {
+    return array$$1[0][0];
+  }
+}
+
+function TimelineModule$$$add(time, task, _arg1$$1) {
+  const array$$2 = _arg1$$1;
+  const i = TimelineModule$$$findAppendPosition(time, array$$2) | 0;
+
+  if (i === -1) {
+    array$$2.splice(0, 0, [time, task]);
+  } else {
+    const patternInput = array$$2[i];
+
+    if ((0, _Util.equals)(patternInput[0], time)) {
+      array$$2[i] = [patternInput[0], (0, _task.append)(patternInput[1], task)];
+    } else {
+      array$$2.splice(i + 1, 0, [time, task]);
+    }
+  }
+}
+
+function TimelineModule$$$removeTasks(time$$1, _arg1$$2) {
+  const array$$3 = _arg1$$2;
+  return (0, _task.appendArray)((0, _Array.ofSeq)((0, _Seq.map)(function mapping(tuple) {
+    return tuple[1];
+  }, array$$3.splice(0, TimelineModule$$$findAppendPosition(time$$1, array$$3) + 1)), Array));
+}
+
 function return$0027(timer, clock) {
-  return [new _Types.FSharpRef(null), timer, clock, (0, _timeline.empty)()];
+  return [new _Types.FSharpRef(null), timer, clock, TimelineModule$$$empty()];
 }
 
 function scheduleNextRun(scheduler) {
   const timeline = scheduler[3];
   const ref = scheduler[0];
-  const matchValue = [(0, _timeline.nextArrival)(timeline), ref.contents];
+  const matchValue = [TimelineModule$$$nextArrival(timeline), ref.contents];
 
   if (matchValue[0] == null) {
     if (matchValue[1] != null) {
@@ -56,9 +120,9 @@ function setNextRun(nextArrival$$2, scheduler$$1) {
   const timeline$$1 = scheduler$$1[3];
   const ref$$1 = scheduler$$1[0];
   const clock$$1 = scheduler$$1[2];
-  const task = (0, _task.return$0027)(function (_arg1$$1) {
-    if (_arg1$$1.tag === 1) {
-      const err = _arg1$$1.fields[1];
+  const task$$1 = (0, _task.return$0027)(function (_arg1$$4) {
+    if (_arg1$$4.tag === 1) {
+      const err = _arg1$$4.fields[1];
 
       if (!false) {
         debugger;
@@ -66,65 +130,65 @@ function setNextRun(nextArrival$$2, scheduler$$1) {
 
       throw err;
     } else {
-      const s = _arg1$$1.fields[0][1];
+      const s = _arg1$$4.fields[0][1];
       ref$$1.contents = null;
-      (0, _task.run)((0, _timeline.removeTasks)((0, _clock.now)(clock$$1), timeline$$1));
+      (0, _task.run)(TimelineModule$$$removeTasks((0, _clock.now)(clock$$1), timeline$$1));
       scheduleNextRun(scheduler$$1);
       return null;
     }
   });
   const delay = (0, _time.DelayModule$$$fromTo)((0, _clock.now)(clock$$1), nextArrival$$2);
-  return (0, _timer.setTimer)(task, delay, timer$$1);
+  return (0, _timer.setTimer)(task$$1, delay, timer$$1);
 }
 
-function add(time, period, task$$1, cancelRef, scheduler$$2) {
+function add(time$$2, period, task$$2, cancelRef, scheduler$$2) {
   const timeline$$2 = scheduler$$2[3];
-  let task$$2;
+  let task$$3;
 
   if (period == null) {
-    task$$2 = task$$1;
+    task$$3 = task$$2;
   } else {
     const period$$1 = period;
-    task$$2 = (0, _task.append)(task$$1, (0, _task.return$0027)(function (_arg1$$2) {
-      if (_arg1$$2.tag === 1) {
+    task$$3 = (0, _task.append)(task$$2, (0, _task.return$0027)(function (_arg1$$5) {
+      if (_arg1$$5.tag === 1) {
         if (!false) {
           debugger;
         }
 
         return null;
       } else {
-        const time$$1 = _arg1$$2.fields[0][0];
-        const time$$2 = (0, _time.add)(time$$1, period$$1);
-        add(time$$2, period$$1, task$$1, cancelRef, scheduler$$2);
+        const time$$3 = _arg1$$5.fields[0][0];
+        const time$$4 = (0, _time.add)(time$$3, period$$1);
+        add(time$$4, period$$1, task$$2, cancelRef, scheduler$$2);
         return null;
       }
     }));
   }
 
-  const patternInput = (0, _task.Cancelable$$$extend)(task$$2);
-  const task$$4 = (0, _task.map)(function f$$3() {
-    return time;
-  }, patternInput[0]);
+  const patternInput$$1 = (0, _task.Cancelable$$$extend)(task$$3);
+  const task$$5 = (0, _task.map)(function f$$3() {
+    return time$$2;
+  }, patternInput$$1[0]);
   const f$$4 = cancelRef.contents;
   f$$4();
-  cancelRef.contents = patternInput[1];
-  (0, _timeline.add)(time, task$$4, timeline$$2);
+  cancelRef.contents = patternInput$$1[1];
+  TimelineModule$$$add(time$$2, task$$5, timeline$$2);
 }
 
-function schedule(delay$$1, period$$2, task$$5, scheduler$$3) {
+function schedule(delay$$1, period$$2, task$$6, scheduler$$3) {
   const clock$$2 = scheduler$$3[2];
   const now = (0, _clock.now)(clock$$2);
-  let time$$3;
+  let time$$5;
 
   if (delay$$1 != null) {
     const delay$$2 = delay$$1;
-    time$$3 = (0, _time.add)(now, delay$$2);
+    time$$5 = (0, _time.add)(now, delay$$2);
   } else {
-    time$$3 = now;
+    time$$5 = now;
   }
 
   const cancelRef$$1 = new _Types.FSharpRef(_disposable.empty);
-  add(time$$3, period$$2, task$$5, cancelRef$$1, scheduler$$3);
+  add(time$$5, period$$2, task$$6, cancelRef$$1, scheduler$$3);
   scheduleNextRun(scheduler$$3);
   return (0, _disposable.return$0027)(function () {
     const f$$6 = cancelRef$$1.contents;
