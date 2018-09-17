4de3f610fc30009d9363db086b90cc0b6cc2c3cc
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-09-17 15:32:35 +0400

    sync

--- core/time-io.fs
+++ core/time-io.fs
@@ -2,21 +2,23 @@ module Sakhe.TimeIO
 open Fable.Core
 open Sakhe.S
 
-// type [<Erase>] T<'a> = private TimeIO of IO<I, O, 'a>
-// and I = TaskIO.I<Time.T>
-// and O =
-//     private
-//     | Run of T<unit>
-//     | Dispose of Disposable.T
-//     | Delay of Time.Delay * T<unit>
+type O =
+    private
+    | Run of (I<IO.TryCatch<Time.T>> -> O<O, unit>)
+    | Dispose of Disposable.T
+    | Delay of Time.Delay * (I<IO.TryCatch<Time.T>> -> O<O, unit>)
 
-
-// let return' f = TimeIO << IO <| f
-
-// // module O =
-// //     let Delay a f = Delay <| (Time.Delay.return' a, return' f)
-// //     let Run f = Run << return' <| f
-// //     let Dispose d = Dispose d
+let rec run
+    now delay io =
+    let t = Time.add delay now
+    let io = fun i ->
+        io i
+        |> O.map (function
+            | Run io -> run t (Time.Delay.return' 0) io
+            | Dispose d -> d
+            | Delay (delay, io) -> run t delay io) id
+    let (rez, disposable) = IO.run (I.Of t) io
+    disposable
 
 // open Fable.Import
 
@@ -33,25 +35,17 @@ open Sakhe.S
 
 // let rec run (delay: Time.Delay) (TimeIO io) =
 //     ()
+module O =
+    let delay d io = Delay (Time.Delay.return' d, IO.return' io)
+    let run io = Run << IO.return' <| io
+    let dispose d = Dispose d
 
-// function
-// | I.Run a -> Pith <| fun o ->
-//     o << O.Delay 100 <| function
-//         | I.Run a -> Pith ignore
-//         | I.Exn (a, err) -> Pith ignore
-
-//     o << O.Run <| function
-//         | I.Run a -> Pith <| fun o ->
-//             let rec t = function
-//                 | I.Run a -> Pith <| fun o ->
-//                     printfn "tick"
-//                     o << O.Delay 100 <| t
-//                 | I.Exn (a, err) -> Pith ignore
-//             o << O.Delay 100 <| t
-
-//         | I.Exn (a, err) -> Pith ignore
-
-//     o << O.Dispose <| Disposable.empty
-// | I.Exn (a, err) -> Pith <| fun o ->
-//     failwith "never"
-// |> (run (Time.Delay.return' 0) << return')
+let see =
+    fun o -> function
+    | IO.Try a ->
+        o << O.delay 100 <| fun o -> function
+            | IO.Try a -> ()
+            | IO.Catch (a, err) -> ()
+    | IO.Catch (a, err) -> ()
+    |> IO.return'
+    |> run (Time.return' 0.) (Time.Delay.return' 0)
