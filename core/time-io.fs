7b39973c3cd266ad4393386014b310ab8181c142
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-09-18 23:57:39 +0400

    sync

--- core/time-io.fs
+++ core/time-io.fs
@@ -11,40 +11,30 @@ type O =
 exception CancellationException
 
 open Fable.Import
-open Fable.Import
+
 let private setTask delay task =
     let token = JS.setTimeout task (Time.Delay.unbox delay)
     Disposable.return' <| fun () -> JS.clearTimeout token
 
 let rec run now delay io =
     let now = Time.add delay now
-    let io2  = IO.return' <| fun o -> function
-        | IO.Catch ((), _)
-        | IO.Try () ->
-            let mapO = function
-                | Run io ->
-                    let see = io <| IO.Try now
-                    run now Time.Delay.zero io
-                | Dispose d -> d
-                | Delay (delay, io) -> run now delay io
-
-            let o = (O.return' (fun list a -> a :: list) []) |> O.contraMap mapO
-            Pith.run o << io <| IO.Try now
-
-
     let clearTimeOut = setTask delay <| fun () ->
         let o = O.return' Disposable.append Disposable.empty
+
         IO.run o now <| fun i -> Pith <| fun o ->
-            let mapO = function
+            let foldO = fun d -> function
                 | Run io ->
-                    let see = io <| IO.Try now
                     run now Time.Delay.zero io
                 | Dispose d -> d
                 | Delay (delay, io) -> run now delay io
 
-            let o2 = (O.return' (fun list a -> a :: list) [])
+            let o2 =
+                (O.return' (fun list a -> a :: list) [])
+
+
             Pith.run o2 (io i)
-            o <| List.fold mapO Disposable.empty o2.Value
+
+            o <| List.fold foldO Disposable.empty o2.Value
     clearTimeOut
 
 
