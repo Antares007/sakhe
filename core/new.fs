6eb393e3986b89d4417e61533d9f70b845e29c7b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-09-24 20:23:01 +0400

    sync

--- core/new.fs
+++ core/new.fs
@@ -2,53 +2,52 @@ module Sakhe.Scheduler
 open System
 open Fable.Core
 
+type On<'a> =
+    | Event of Time.T * 'a
+    | End   of Time.T
+    | Error of Time.T * exn
+type TryCatch =
+    | Try
+    | Catch of exn
 
-type [<Erase>] T<'a> = private Stream of IO.T<Time.T, O<'a>, TaskIO.T<Sink.T<'a>>>
+type [<Erase>] T<'a> = private Stream of Pith.T<On<'a>, IO.T<Time.T, O<'a>, IO.T<TryCatch, IDisposable, unit>>>
 and O<'a> =
     private
     | Now of T<'a>
     | Delay of Time.Delay * T<'a>
     | Periodic of Time.Delay * T<'a>
 
-let return' f = Stream << IO.return' <| f
+let return' f = Stream << Pith.return' <| (IO.return' << f)
 
 module O =
     let Now f = Now << return' <| f
     let delay delay f = Delay (Time.Delay.return' delay, return' f)
     let periodic delay f = Periodic (Time.Delay.return' delay, return' f)
 
-let run2 o now (Stream io) =
-    let io = IO.return' <| fun () o ->
-        o ""
-        1
-    ()
+// let run2 o now (Stream io) =
+//     let io = IO.return' <| fun () o ->
+//         o ""
+//         1
+//     ()
 
-let inline run o now (Stream io) =
-    let o' =
-        O.return'
-            (fun acc -> function
-                | Now (Stream io) -> io :: acc
-                | x -> O.put x o; acc)
-            []
-    Seq.fold
-        (fun acc io -> acc + (IO.run now o' io))
-        (IO.run now o' io)
-        o'.Value
+// let inline run o now (Stream io) =
+//     let o' =
+//         O.return'
+//             (fun acc -> function
+//                 | Now (Stream io) -> io :: acc
+//                 | x -> O.put x o; acc)
+//             []
+//     Seq.fold
+//         (fun acc io -> acc + (IO.run now o' io))
+//         (IO.run now o' io)
+//         o'.Value
 
 
-let see = return' <| fun t o ->
-    o << Now << return' <| fun t o ->
-        TaskIO.return' <| fun i o ->
-            match i with
-            | TaskIO.Catch (sink, err) ->
-                Sink.Send.error t err sink
-            | TaskIO.Try sink ->
-                o <| Disposable.empty
-                Sink.Send.event t 1 sink
-    TaskIO.return' <| fun i o ->
+let see = return' <| fun sink now o ->
+    IO.return' <| fun i o ->
         match i with
-        | TaskIO.Catch (sink, err) ->
-            Sink.Send.error t err sink
-        | TaskIO.Try sink ->
+        | Try ->
             o <| Disposable.empty
-            Sink.Send.event t 1 sink
+            sink <| Event (now, 1)
+        | Catch err ->
+            sink <| Error (now, err)
--- out/modules/new.js
+++ out/modules/new.js
@@ -7,73 +7,59 @@ exports.return$0027 = return$0027;
 exports.O$$$Now = O$$$Now;
 exports.O$$$delay = O$$$delay;
 exports.O$$$periodic = O$$$periodic;
-exports.run2 = run2;
-exports.see = exports.O$00601 = void 0;
+exports.see = exports.O$00601 = exports.TryCatch = exports.On$00601 = void 0;
 
 var _Types = require("./fable-core.2.0.0-beta-005/Types");
 
+var _Util = require("./fable-core.2.0.0-beta-005/Util");
+
 var _io = require("./io");
+
+var _pith = require("./pith");
 
 var _time = require("./time");
 
 var _disposable = require("./disposable");
 
-var _sink = require("./sink");
-
-var _taskIo = require("./task-io");
-
+const On$00601 = (0, _Types.declare)(function On$00601(tag, name, ...fields) {
+  _Types.Union.call(this, tag, name, ...fields);
+}, _Types.Union);
+exports.On$00601 = On$00601;
+const TryCatch = (0, _Types.declare)(function TryCatch(tag, name, ...fields) {
+  _Types.Union.call(this, tag, name, ...fields);
+}, _Types.Union);
+exports.TryCatch = TryCatch;
 const O$00601 = (0, _Types.declare)(function O$00601(tag, name, ...fields) {
   _Types.Union.call(this, tag, name, ...fields);
 }, _Types.Union);
 exports.O$00601 = O$00601;
 
 function return$0027(f) {
-  return (0, _io.return$0027)(f);
-}
-
-function O$$$Now(f$$2) {
-  return new O$00601(0, "Now", return$0027(f$$2));
-}
-
-function O$$$delay(delay, f$$4) {
-  return new O$00601(1, "Delay", (0, _time.DelayModule$$$return$0027)(delay), return$0027(f$$4));
-}
-
-function O$$$periodic(delay$$1, f$$5) {
-  return new O$00601(2, "Periodic", (0, _time.DelayModule$$$return$0027)(delay$$1), return$0027(f$$5));
-}
-
-function run2(o, now, _arg1) {
-  const io = _arg1;
-  const io$$1 = (0, _io.return$0027)(function (unitVar0, o$$1) {
-    o$$1("");
-    return 1;
+  return (0, _pith.return$0027)(function ($arg$$2) {
+    return (0, _io.return$0027)((0, _Util.uncurry)(2, (0, _Util.partialApply)(2, f, [$arg$$2])));
   });
 }
 
-const see = return$0027(function (t, o$$2) {
-  o$$2(new O$00601(0, "Now", return$0027(function (t$$1, o$$3) {
-    return (0, _taskIo.return$0027)(function (i, o$$4) {
-      if (i.tag === 0) {
-        const sink$$1 = i.fields[0];
-        o$$4(_disposable.empty);
-        (0, _sink.Send$$$event)(t$$1, 1, sink$$1);
-      } else {
-        const sink = i.fields[0];
-        const err = i.fields[1];
-        (0, _sink.Send$$$error)(t$$1, err, sink);
-      }
-    });
-  })));
-  return (0, _taskIo.return$0027)(function (i$$1, o$$5) {
-    if (i$$1.tag === 0) {
-      const sink$$3 = i$$1.fields[0];
-      o$$5(_disposable.empty);
-      (0, _sink.Send$$$event)(t, 1, sink$$3);
+function O$$$Now(f$$3) {
+  return new O$00601(0, "Now", return$0027(f$$3));
+}
+
+function O$$$delay(delay, f$$5) {
+  return new O$00601(1, "Delay", (0, _time.DelayModule$$$return$0027)(delay), return$0027(f$$5));
+}
+
+function O$$$periodic(delay$$1, f$$6) {
+  return new O$00601(2, "Periodic", (0, _time.DelayModule$$$return$0027)(delay$$1), return$0027(f$$6));
+}
+
+const see = return$0027(function (sink, now, o) {
+  return (0, _io.return$0027)(function (i, o$$1) {
+    if (i.tag === 1) {
+      const err = i.fields[0];
+      sink(new On$00601(2, "Error", now, err));
     } else {
-      const sink$$2 = i$$1.fields[0];
-      const err$$1 = i$$1.fields[1];
-      (0, _sink.Send$$$error)(t, err$$1, sink$$2);
+      o$$1(_disposable.empty);
+      sink(new On$00601(0, "Event", now, 1));
     }
   });
 });
