b635c63b8b8dc3dc383b8e426da2b6749e25539f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-09-28 16:12:54 +0400

    stream

--- core/stream.fs
+++ core/stream.fs
@@ -1,24 +1,25 @@
 module Sakhe.Stream
 open System
-open System.Diagnostics.Tracing
 
-type T<'a> = Stream of IO.T<Sink.T<'a>, O<'a>, unit>
-and O<'a> =
-    | Now of 'a
-    | Delay of Time.Delay * 'a
-    | Never
-    | Empty
-    | ThrowError of exn
+type T<'a> = Stream of IO.T<Sink.T<'a>, O, unit>
+and O =
+    | Now of IO.T<Time.T, O, unit>
+    | Delay of Time.Delay * IO.T<Time.T, O, unit>
+    | Dispose of (unit -> Time.T) * IDisposable
 
-let now a sink = Scheduler.return' <| fun now o ->
-    o << Scheduler.O.delay 0 <| fun now o ->
+
+let now a = (Stream << IO.return') <| fun sink o ->
+    o <| Delay (Time.Delay.zero, IO.return' <| fun now o ->
         try
             Sink.Send.event now a sink
             Sink.Send.end' now sink
         with err ->
-            Sink.Send.error now err sink
+            Sink.Send.error now err sink)
+
 
 let rec run tf timer io =
+
     let disposable = Scheduler.run tf timer << Scheduler.return' <| fun i o ->
         IO.run i (O.proxy o) io
+
     disposable
