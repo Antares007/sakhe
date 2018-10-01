aa25d56d8b4f9de4a64d286296b43fc643bb3b84
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-10-01 15:20:50 +0400

    sync

--- core/timeline.fs
+++ core/timeline.fs
@@ -1,12 +1,63 @@
 module Sakhe.TimeLine
 open System
+open Sakhe
 
-type T<'a, 'b when 'a: comparison> = private | TimeLine of ('a [] * Map<'a, 'b>)
+type T<'a, 'b, 'c> =
+    private
+    | TimeLine of ('a * ('b * 'c) list) list
+    | Abo of Abo.T<unit, unit, (('a * 'b) * 'c)>
 
-let return' map =
-    let line = map |> Map.toSeq |> Seq.map fst |> Seq.sort |> Seq.toArray
-    if Array.isEmpty line then None
-    else Some <| TimeLine (line, map)
+let return' a io =
+    let o =
+        fun m ((a, b), c) ->
+            match Map.tryFind a m with
+            | None -> m |> Map.add a [b, c]
+            | Some o -> m |> Map.add a ((b, c)::o)
+        |> O.return' <| Map.empty
+    Abo.run a o io
+    if o.Value.Count = 0
+    then None
+    else Some<<TimeLine<<(Seq.fold (fun l a -> a::l) [])<<Seq.sortBy fst<<Map.toSeq<|o.Value
+
+let private toAbo = function
+    | TimeLine l ->
+        let see = Abo.return' <| fun () ->
+            let rec go f = function [] -> () | h::t -> f (h); go f t
+            let mutable g = fun o -> ()
+            fun (a, l) ->
+                fun (b, c) ->
+                    let f = g
+                    g <- fun o ->
+                        o ((a, b),c)
+                        f o
+                |> go <| l
+            |> go <| l
+            fun o -> g o
+        see
+    | Abo abo -> abo
+
+let takeUntil now = function
+    | TimeLine tl ->
+
+let rec mappend l r =
+    match l, r with
+    | TimeLine _, Abo _ -> mappend (Abo <| toAbo l) r
+    | Abo _, TimeLine _ -> mappend l (Abo <| toAbo r)
+    | Abo l, Abo r -> Abo <| Abo.mappend Unit.mappend l r
+    | TimeLine l, TimeLine r ->
+        let rec go o l r =
+            match l, r with
+            | [], [] -> o
+            | l, [] -> o@l
+            | [], r -> o@r
+            | l::lt, r::rt ->
+                if fst l < fst r then go (r::l::o) lt rt else
+                if fst l > fst r then go (l::r::o) lt rt else
+                                      go ((fst l, snd l @ snd r)::o) lt rt
+        TimeLine <| go [] l r
+
+
+
 
 let nextArrival (TimeLine (a, _)) = Array.head a
 
