ec17547d6858071e8b5d8f6ca0b1b4d7beb97c83
Archil Bolkvadze <a.bolkvadze@gmail.com>
2018-10-01 17:40:39 +0400

    sync

--- core/timeline.fs
+++ core/timeline.fs
@@ -22,22 +22,52 @@ let return' a io =
 let private toAbo = function
     | TimeLine l ->
         let see = Abo.return' <| fun () ->
-            let rec go f = function [] -> () | h::t -> f (h); go f t
-            let mutable g = fun o -> ()
+            let mutable g = ignore
             fun (a, l) ->
                 fun (b, c) ->
                     let f = g
                     g <- fun o ->
                         o ((a, b),c)
                         f o
-                |> go <| l
-            |> go <| l
-            fun o -> g o
+                |> List.iter <| l
+            |> List.iter <| l
+            g
         see
     | Abo abo -> abo
 
+
 let takeUntil now = function
     | TimeLine tl ->
+        let mutable g = ignore
+        let mutable tail = None
+        let rec go = function
+            | [] -> ()
+            | (a, l)::t ->
+                if a < now then
+                    fun (b, c) ->
+                        let f = g
+                        g <- fun o ->
+                            o ((a,b),c)
+                            f o
+                    |> List.iter <| l
+                    go t
+                else
+                    tail <- Some << TimeLine <| (a, l)::t
+        go tl
+        (Abo.return' <| fun () o -> g o), tail
+    | Abo abo ->
+        let o =
+            fun (l, r) b ->
+                if fst (fst b) < now
+                then
+                    (fun o -> l o; o b), r
+                else
+                    l, (fun o -> r o; o b)
+            |> O.return' <| (ignore, ignore)
+        Abo.run () o abo
+        let (f, g) = o.Value
+        let tail = (return' () << Abo.return'<| fun () o -> g o)
+        (Abo.return' <| fun () o -> f o;()), tail
 
 let rec mappend l r =
     match l, r with
@@ -55,61 +85,3 @@ let rec mappend l r =
                 if fst l > fst r then go (l::r::o) lt rt else
                                       go ((fst l, snd l @ snd r)::o) lt rt
         TimeLine <| go [] l r
-
-
-
-
-let nextArrival (TimeLine (a, _)) = Array.head a
-
-let private mergea l r =
-    let ll = Array.length l
-    let rl = Array.length r
-    let to' = Math.Max (ll, rl) - 1
-    seq {
-        for i = 0 to to' do
-            match (if i < ll then Some (l.[i]) else None), if i < rl then Some (r.[i]) else None with
-            | None, None -> ()
-            | Some l, None -> yield l
-            | None, Some r -> yield r
-            | Some l, Some r when l < r -> yield l; yield r
-            | Some l, Some r when l > r -> yield r; yield l
-            | Some l, Some _ -> yield l
-    } |> Seq.toArray
-
-let private mergem mappend l r =
-    seq {
-        yield! l |> Map.toSeq
-            |> Seq.map (fun (lk, lv) ->
-                        match Map.tryFind lk r with
-                        | None -> (lk, lv)
-                        | Some rv -> (lk, mappend lv rv))
-        yield! r |> Map.toSeq
-            |> Seq.filter (fun (k, _) -> not (l |> Map.containsKey k))
-    } |> Map.ofSeq
-
-let mappend mappend (TimeLine (la, lm)) (TimeLine (ra, rm)) =
-    TimeLine (mergea la ra, mergem mappend lm rm)
-
-let private findAppendPosition (a: 'a) (sortedArray: 'a[]) =
-    let rec go l r =
-        if l < r then
-            let m = (l + r) / 2
-            if sortedArray.[m] > a then go l m
-            else go (m + 1) r
-        else l - 1
-    go 0 (Array.length sortedArray)
-
-let takeUntil now tl =
-    let (TimeLine (timeLine, timeMap)) = tl
-    match findAppendPosition now timeLine with
-    | -1 -> (Seq.empty, Some tl)
-    | i ->
-        let iPlus1 = i + 1
-        let tl =
-            if iPlus1 = Array.length timeLine then None
-            else Some << TimeLine <| (
-                  timeLine |> Array.skip (iPlus1)
-                , timeLine |> Seq.skip (iPlus1) |> Seq.map (fun t -> (t, timeMap.[t])) |> Map.ofSeq)
-
-        let see = timeLine |> Seq.take (iPlus1) |> Seq.map (fun t -> (t, timeMap.[t]))
-        see, tl
