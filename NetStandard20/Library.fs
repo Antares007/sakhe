7ada879223a572aa1d8629c51b4b5025789102fd
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-01-08 18:52:43 +0400

    remove paket

--- NetStandard20/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace NetStandard20
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- NetStandard20/js/Library.js
+++
@@ -1,4 +0,0 @@
-import { toConsole, printf } from "./fable-core/String.js";
-export function hello(name) {
-  toConsole(printf("Hello %s"))(name);
-}
